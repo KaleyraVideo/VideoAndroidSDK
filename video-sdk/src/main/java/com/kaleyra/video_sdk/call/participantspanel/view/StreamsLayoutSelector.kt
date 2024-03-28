ec544b45a1a7adc66241fe2fe8d9c7b53b312331
Stefano Brusadelli <stefano.brusadelli@kaleyra.com>
2024-03-28 12:57:12 +0100

    refactor: move StreamsLayoutSelector

---
+++ video-sdk/src/main/java/com/kaleyra/video_sdk/call/participantspanel/view/StreamsLayoutSelector.kt
@@ -0,0 +1,71 @@
+package com.kaleyra.video_sdk.call.participantspanel.view
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import com.kaleyra.video_sdk.R
+import com.kaleyra.video_sdk.call.participantspanel.StreamsLayout
+
+@Composable
+internal fun StreamsLayoutSelector(
+    streamsLayout: StreamsLayout,
+    onLayoutClick: (streamsLayout: StreamsLayout) -> Unit
+) {
+    Row(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
+        Button(
+            shape = RoundedCornerShape(4.dp),
+            colors = ButtonDefaults.buttonColors(
+                containerColor = MaterialTheme.colorScheme.let { if (streamsLayout == StreamsLayout.Grid) it.primary else it.surfaceVariant },
+                contentColor = MaterialTheme.colorScheme.let { if (streamsLayout == StreamsLayout.Grid) it.onPrimary else it.onSurfaceVariant }
+            ),
+            modifier = Modifier.weight(1f),
+            onClick = { onLayoutClick(StreamsLayout.Grid) }
+        ) {
+            Icon(
+                painter = painterResource(id = R.drawable.ic_kaleyra_participant_panel_grid),
+                contentDescription = null,
+                modifier = Modifier.size(16.dp)
+            )
+            Spacer(Modifier.width(12.dp))
+            Text(stringResource(R.string.kaleyra_participants_panel_grid), fontWeight = FontWeight.SemiBold)
+        }
+
+        Spacer(Modifier.width(14.dp))
+
+        Button(
+            shape = RoundedCornerShape(4.dp),
+            colors = ButtonDefaults.buttonColors(
+                containerColor = MaterialTheme.colorScheme.let { if (streamsLayout == StreamsLayout.Pin) it.primary else it.surfaceVariant },
+                contentColor = MaterialTheme.colorScheme.let { if (streamsLayout == StreamsLayout.Pin) it.onPrimary else it.onSurfaceVariant }
+            ),
+            modifier = Modifier.weight(1f),
+            onClick = { onLayoutClick(StreamsLayout.Pin) }
+        ) {
+            Icon(
+                painter = painterResource(id = R.drawable.ic_kaleyra_participant_panel_pin),
+                contentDescription = null,
+                modifier = Modifier.size(16.dp)
+            )
+            Spacer(Modifier.width(12.dp))
+            Text(stringResource(R.string.kaleyra_participants_panel_pin), fontWeight = FontWeight.SemiBold)
+        }
+    }
+}
