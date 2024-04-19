c6c1616d5cc2c3a0d618805facc8571392e0e4c5
Stefano Brusadelli <stefano.brusadelli@kaleyra.com>
2024-04-19 15:49:15 +0200

    refactor: add badge in camera and mic call actions

--- video-sdk/src/main/java/com/kaleyra/video_sdk/call/callactionnew/CameraAction.kt
+++ video-sdk/src/main/java/com/kaleyra/video_sdk/call/callactionnew/CameraAction.kt
@@ -1,6 +1,7 @@
 package com.kaleyra.video_sdk.call.callactionnew
 
 import android.content.res.Configuration
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -9,19 +10,25 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.kaleyra.video_sdk.R
 import com.kaleyra.video_sdk.theme.KaleyraM3Theme
+import com.kaleyra.video_sdk.theme.KaleyraTheme
 
 @Composable
 internal fun CameraAction(
     checked: Boolean,
     onCheckedChange: (Boolean) -> Unit,
-    modifier: Modifier = Modifier
+    modifier: Modifier = Modifier,
+    warning: Boolean = false,
+    error: Boolean = false
 ) {
     CallToggleAction(
         modifier = modifier,
         icon = painterResource(id = if (checked) R.drawable.ic_kaleyra_call_sheet_enable_camera else R.drawable.ic_kaleyra_call_sheet_disable_camera),
         contentDescription = stringResource(id = if (checked) R.string.kaleyra_call_sheet_description_enable_camera else R.string.kaleyra_call_sheet_description_disable_camera),
         checked = checked,
-        onCheckedChange = onCheckedChange
+        onCheckedChange = onCheckedChange,
+        badgeText = if (warning || error) "!" else null,
+        badgeBackgroundColor = if (error) MaterialTheme.colorScheme.error else KaleyraTheme.colors.warning,
+        badgeContentColor = if (error) MaterialTheme.colorScheme.onError else KaleyraTheme.colors.onWarning
     )
 }
 
--- video-sdk/src/main/java/com/kaleyra/video_sdk/call/callactionnew/MicAction.kt
+++ video-sdk/src/main/java/com/kaleyra/video_sdk/call/callactionnew/MicAction.kt
@@ -1,27 +1,35 @@
 package com.kaleyra.video_sdk.call.callactionnew
 
 import android.content.res.Configuration
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.kaleyra.video_sdk.R
 import com.kaleyra.video_sdk.theme.KaleyraM3Theme
+import com.kaleyra.video_sdk.theme.KaleyraTheme
 
 @Composable
 internal fun MicAction(
     checked: Boolean,
     onCheckedChange: (Boolean) -> Unit,
-    modifier: Modifier = Modifier
+    modifier: Modifier = Modifier,
+    warning: Boolean = false,
+    error: Boolean = false
 ) {
     CallToggleAction(
         modifier = modifier,
         icon = painterResource(id = if (checked) R.drawable.ic_kaleyra_call_sheet_enable_microphone else R.drawable.ic_kaleyra_call_sheet_disable_microphone),
         contentDescription = stringResource(id = if (checked) R.string.kaleyra_call_sheet_description_enable_microphone else R.string.kaleyra_call_sheet_description_disable_microphone),
         checked = checked,
-        onCheckedChange = onCheckedChange
+        onCheckedChange = onCheckedChange,
+        badgeText = if (warning || error) "!" else null,
+        badgeBackgroundColor = if (error) MaterialTheme.colorScheme.error else KaleyraTheme.colors.warning,
+        badgeContentColor = if (error) MaterialTheme.colorScheme.onError else KaleyraTheme.colors.onWarning
     )
 }
 
