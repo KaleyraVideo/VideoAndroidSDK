24ed27f29102e470b20c939223b5e75ec303ff4f
Stefano Brusadelli <stefano.brusadelli@kaleyra.com>
2024-05-13 12:22:00 +0200

    refactor: new call action model

--- video-sdk/src/main/java/com/kaleyra/video_sdk/call/screennew/CallActionUI.kt
+++ video-sdk/src/main/java/com/kaleyra/video_sdk/call/screennew/CallActionUI.kt
@@ -1,16 +1,22 @@
 package com.kaleyra.video_sdk.call.screennew
 
 import androidx.compose.runtime.Immutable
+import androidx.compose.runtime.Stable
 import com.kaleyra.video_sdk.call.audiooutput.model.AudioDeviceUi
+import java.util.UUID
 
+@Stable
 interface CallActionUI {
+    val id: String
     val isEnabled: Boolean
 }
 
+@Stable
 interface ToggleableCallAction : CallActionUI {
     val isToggled: Boolean
 }
 
+@Stable
 interface InputCallAction : ToggleableCallAction {
     val state: State
 
@@ -21,39 +27,68 @@ interface InputCallAction : ToggleableCallAction {
     }
 }
 
-@Immutable
-data class HangUpAction(override val isEnabled: Boolean = true) : CallActionUI
+@Stable
+interface NotifiableCallAction: CallActionUI {
+    val notificationCount: Int
+}
 
 @Immutable
-data class FlipCameraAction(override val isEnabled: Boolean = true) : CallActionUI
+data class HangUpAction(
+    override val id: String = UUID.randomUUID().toString(),
+    override val isEnabled: Boolean = true
+) : CallActionUI
+
+@Immutable
+data class FlipCameraAction(
+    override val id: String = UUID.randomUUID().toString(),
+    override val isEnabled: Boolean = true
+) : CallActionUI
 
 @Immutable
 data class AudioAction(
+    override val id: String = UUID.randomUUID().toString(),
     val audioDevice: AudioDeviceUi = AudioDeviceUi.LoudSpeaker,
     override val isEnabled: Boolean = true
 ) : CallActionUI
 
 @Immutable
-data class ChatAction(override val isEnabled: Boolean = true) : CallActionUI
+data class ChatAction(
+    override val id: String = UUID.randomUUID().toString(),
+    override val isEnabled: Boolean = true,
+    override val notificationCount: Int = 0
+) : NotifiableCallAction
 
 @Immutable
-data class FileShareAction(override val isEnabled: Boolean = true) : CallActionUI
+data class FileShareAction(
+    override val id: String = UUID.randomUUID().toString(),
+    override val isEnabled: Boolean = true,
+    override val notificationCount: Int = 0
+) : NotifiableCallAction
 
 @Immutable
-data class WhiteboardAction(override val isEnabled: Boolean = true) : CallActionUI
+data class WhiteboardAction(
+    override val id: String = UUID.randomUUID().toString(),
+    override val isEnabled: Boolean = true,
+    override val notificationCount: Int = 0
+) : NotifiableCallAction
 
 @Immutable
-data class VirtualBackgroundAction(override val isEnabled: Boolean = true) : CallActionUI
+data class VirtualBackgroundAction(
+    override val id: String = UUID.randomUUID().toString(),
+    override val isEnabled: Boolean = true
+) : CallActionUI
 
 @Immutable
 data class MicAction(
+    override val id: String = UUID.randomUUID().toString(),
     override val state: InputCallAction.State = InputCallAction.State.Ok,
     override val isEnabled: Boolean = true,
-    override val isToggled: Boolean = false
+    override val isToggled: Boolean = false,
 ) : InputCallAction
 
 @Immutable
 data class CameraAction(
+    override val id: String = UUID.randomUUID().toString(),
     override val state: InputCallAction.State = InputCallAction.State.Ok,
     override val isEnabled: Boolean = true,
     override val isToggled: Boolean = false
@@ -61,7 +96,7 @@ data class CameraAction(
 
 @Immutable
 data class ScreenShareAction(
-    override val state: InputCallAction.State = InputCallAction.State.Ok,
+    override val id: String = UUID.randomUUID().toString(),
     override val isEnabled: Boolean = true,
     override val isToggled: Boolean = false
-) : InputCallAction
+) : ToggleableCallAction
