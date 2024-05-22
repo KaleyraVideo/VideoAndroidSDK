7b67c48848e71e0cfd6ac09375422ba8822ceb84
fede87 <federicomarin87@gmail.com>
2024-05-22 11:34:22 +0200

    fix: attempt to fix flaky tests

--- video-common-ui/src/test/java/com/kaleyra/video_common_ui/KaleyraVideoFacadeTest.kt
+++ video-common-ui/src/test/java/com/kaleyra/video_common_ui/KaleyraVideoFacadeTest.kt
@@ -6,15 +6,30 @@ import com.kaleyra.video_utils.ContextRetainer
 import io.mockk.every
 import io.mockk.mockk
 import io.mockk.mockkObject
-import kotlinx.coroutines.test.runTest
+import io.mockk.mockkStatic
+import kotlinx.coroutines.ExecutorCoroutineDispatcher
+import kotlinx.coroutines.Runnable
+import kotlinx.coroutines.asCoroutineDispatcher
 import org.junit.Assert
 import org.junit.Before
 import org.junit.Test
+import java.util.concurrent.Executor
+import java.util.concurrent.Executors
+import kotlin.coroutines.CoroutineContext
 
 class KaleyraVideoFacadeTest {
 
     @Before
     fun setup() {
+        mockkStatic(Executors::class)
+        mockkStatic("kotlinx.coroutines.ExecutorsKt")
+        every { Executors.newSingleThreadExecutor() } returns mockk {
+            every { this@mockk.asCoroutineDispatcher() } returns object : ExecutorCoroutineDispatcher() {
+                override val executor: Executor = Executor { }
+                override fun close() = Unit
+                override fun dispatch(context: CoroutineContext, block: Runnable) = block.run()
+            }
+        }
         mockkObject(ContextRetainer)
         every { ContextRetainer.context } returns mockk()
         mockkObject(PhoneActivityClazzProvider)
@@ -22,27 +37,26 @@ class KaleyraVideoFacadeTest {
         every { Collaboration.create(any()) } returns mockk(relaxed = true)
         every { PhoneActivityClazzProvider.getActivityClazzConfiguration() } returns mockk(relaxed = true)
         KaleyraVideo.reset()
-
     }
 
     @Test(expected = Exception::class)
-    fun testConferenceUINotReady() = runTest {
+    fun testConferenceUINotReady() {
         KaleyraVideo.conference
     }
 
     @Test(expected = Exception::class)
-    fun testConversationUINotReady() = runTest {
+    fun testConversationUINotReady() {
         KaleyraVideo.conversation
     }
 
     @Test
-    fun testConferenceUIReady() = runTest {
+    fun testConferenceUIReady() {
         KaleyraVideo.configure(mockk(relaxed = true))
         Assert.assertEquals(true, KaleyraVideo.conference != null)
     }
 
     @Test
-    fun testConversationUIReady() = runTest {
+    fun testConversationUIReady() {
         KaleyraVideo.configure(mockk(relaxed = true))
         Assert.assertEquals(true, KaleyraVideo.conversation != null)
     }
