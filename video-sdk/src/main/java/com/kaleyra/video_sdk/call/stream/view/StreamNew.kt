a816ffcd3c4c6c02671aaad1ed0a2895345f4350
Stefano Brusadelli <stefano.brusadelli@kaleyra.com>
2024-05-24 14:21:58 +0200

    refactor: add StreamNew composable

---
+++ video-sdk/src/main/java/com/kaleyra/video_sdk/call/stream/view/StreamNew.kt
@@ -0,0 +1,60 @@
+package com.kaleyra.video_sdk.call.stream.view
+
+import android.view.ViewGroup
+import androidx.compose.foundation.layout.BoxWithConstraints
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.key
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.unit.coerceIn
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.viewinterop.AndroidView
+import com.kaleyra.video_sdk.call.stream.model.ImmutableView
+import com.kaleyra.video_sdk.call.stream.view.core.StreamViewTestTag
+import com.kaleyra.video_sdk.common.avatar.model.ImmutableUri
+import com.kaleyra.video_sdk.common.avatar.view.Avatar
+
+//const val StreamViewTestTag = "StreamViewTestTag"
+
+@Composable
+internal fun Stream(
+    streamView: ImmutableView?,
+    username: String,
+    avatar: ImmutableUri?,
+    showStreamView: Boolean
+) {
+    if (showStreamView && streamView != null) {
+        key(streamView) {
+            AndroidView(
+                factory = {
+                    streamView.value.also {
+                        val parentView = it.parent as? ViewGroup
+                        parentView?.removeView(it)
+                    }
+                },
+                update = { view ->
+                    val newLayoutParams = view.layoutParams
+                    newLayoutParams.height = ViewGroup.LayoutParams.MATCH_PARENT
+                    newLayoutParams.width = ViewGroup.LayoutParams.MATCH_PARENT
+                    view.layoutParams = newLayoutParams
+                },
+                modifier = Modifier.testTag(StreamViewTestTag)
+            )
+        }
+    } else {
+        BoxWithConstraints(
+            contentAlignment = Alignment.Center,
+            modifier = Modifier.fillMaxSize()
+        ) {
+            val size = (maxWidth / 2).coerceIn(48.dp, 96.dp)
+            Avatar(
+                uri = avatar,
+                text = username[0].uppercase(),
+                modifier = Modifier.size(size)
+            )
+        }
+    }
+}
