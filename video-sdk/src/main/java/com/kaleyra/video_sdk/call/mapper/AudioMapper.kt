8c0c12f0fb1f8275be7d0591fef32d1d0e4c29eb
Stefano Brusadelli <stefano.brusadelli@kaleyra.com>
2024-06-26 15:43:22 +0200

    refactor: add AudioMapper

---
+++ video-sdk/src/main/java/com/kaleyra/video_sdk/call/mapper/AudioMapper.kt
@@ -0,0 +1,35 @@
+package com.kaleyra.video_sdk.call.mapper
+
+import com.kaleyra.video.conference.Input
+import com.kaleyra.video_sdk.call.streamnew.model.core.AudioUi
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.distinctUntilChanged
+import kotlinx.coroutines.flow.filterIsInstance
+import kotlinx.coroutines.flow.flatMapLatest
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.map
+
+internal object AudioMapper {
+    fun StateFlow<Input.Audio?>.mapToAudioUi(): Flow<AudioUi?> =
+        flow {
+            val initialValue = value?.let { audio ->
+                AudioUi(
+                    id = audio.id,
+                    isEnabled = audio.enabled.value
+                )
+            }
+            emit(initialValue)
+
+            val flow = this@mapToAudioUi.filterIsInstance<Input.Audio>()
+            combine(
+                flow.map { it.id },
+                flow.flatMapLatest { it.enabled },
+            ) { id, enabled ->
+                AudioUi(id = id, isEnabled = enabled)
+            }.collect {
+                emit(it)
+            }
+        }.distinctUntilChanged()
+}
