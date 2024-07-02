d48af8cc71b0d11763677bd6b00705e8ae2373b4
Stefano Brusadelli <stefano.brusadelli@kaleyra.com>
2024-07-02 16:04:58 +0200

    refactor: use LazyHorizontalGrid in VSheetDragContent

--- video-sdk/src/main/java/com/kaleyra/video_sdk/call/bottomsheetnew/sheetdragcontent/VSheetDragContent.kt
+++ video-sdk/src/main/java/com/kaleyra/video_sdk/call/bottomsheetnew/sheetdragcontent/VSheetDragContent.kt
@@ -1,16 +1,15 @@
 package com.kaleyra.video_sdk.call.bottomsheetnew.sheetdragcontent
 
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyHorizontalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.key
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.util.fastForEach
-import com.cheonjaeung.compose.grid.HorizontalGrid
-import com.cheonjaeung.compose.grid.SimpleGridCells
 import com.kaleyra.video_sdk.call.bottomsheetnew.CallSheetItem
-import com.kaleyra.video_sdk.call.bottomsheetnew.SheetCallActionModifier
 import com.kaleyra.video_sdk.call.bottomsheetnew.sheetcontent.sheetitemslayout.SheetItemsSpacing
 import com.kaleyra.video_sdk.call.screennew.CallActionUI
 import com.kaleyra.video_sdk.common.immutablecollections.ImmutableList
@@ -20,6 +19,7 @@ internal val VSheetDragVerticalPadding = SheetItemsSpacing
 
 private const val MaxVSheetDragItems = 5
 
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
 internal fun VSheetDragContent(
     callActions: ImmutableList<CallActionUI>,
@@ -36,34 +36,33 @@ internal fun VSheetDragContent(
     modifier: Modifier = Modifier,
     itemsPerColumn : Int = MaxVSheetDragItems
 ) {
-    val actions = remember(callActions, itemsPerColumn) {
+    val chunkedActions = remember(callActions, itemsPerColumn) {
         callActions.value.chunked(itemsPerColumn, transform = { it.reversed() }).flatten()
     }
-    HorizontalGrid(
-        rows = SimpleGridCells.Fixed(itemsPerColumn),
+
+    LazyHorizontalGrid(
+        rows = GridCells.Fixed(itemsPerColumn),
         horizontalArrangement = Arrangement.spacedBy(VSheetDragHorizontalPadding),
         verticalArrangement = Arrangement.spacedBy(VSheetDragVerticalPadding),
         modifier = modifier
     ) {
-        actions.fastForEach { callAction ->
-            key(callAction.id) {
-                CallSheetItem(
-                    callAction = callAction,
-                    modifier = SheetCallActionModifier,
-                    label = false,
-                    extended = false,
-                    onHangUpClick = onHangUpClick,
-                    onMicToggle = onMicToggled,
-                    onCameraToggle = onCameraToggled,
-                    onScreenShareToggle = onScreenShareToggle,
-                    onFlipCameraClick = onFlipCameraClick,
-                    onAudioClick = onAudioClick,
-                    onChatClick = onChatClick,
-                    onFileShareClick = onFileShareClick,
-                    onWhiteboardClick = onWhiteboardClick,
-                    onVirtualBackgroundClick = onVirtualBackgroundClick
-                )
-            }
+        items(key = { it.id }, items = chunkedActions) { callAction ->
+            CallSheetItem(
+                callAction = callAction,
+                modifier = Modifier.animateItemPlacement(),
+                label = false,
+                extended = false,
+                onHangUpClick = onHangUpClick,
+                onMicToggle = onMicToggled,
+                onCameraToggle = onCameraToggled,
+                onScreenShareToggle = onScreenShareToggle,
+                onFlipCameraClick = onFlipCameraClick,
+                onAudioClick = onAudioClick,
+                onChatClick = onChatClick,
+                onFileShareClick = onFileShareClick,
+                onWhiteboardClick = onWhiteboardClick,
+                onVirtualBackgroundClick = onVirtualBackgroundClick
+            )
         }
     }
 }
