0570447af487b029eaf66604bf118c387b2af0a5
Stefano Brusadelli <stefano.brusadelli@kaleyra.com>
2024-07-08 15:10:58 +0200

    refactor: move old call screen code commented to new call screen

--- video-sdk/src/main/java/com/kaleyra/video_sdk/call/screennew/CallScreen.kt
+++ video-sdk/src/main/java/com/kaleyra/video_sdk/call/screennew/CallScreen.kt
@@ -1,9 +1,6 @@
 package com.kaleyra.video_sdk.call.screennew
 
-import androidx.compose.animation.AnimatedContent
-import androidx.compose.animation.animateContentSize
-import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.foundation.layout.Box
+import android.util.Rational
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.WindowInsetsSides
@@ -11,60 +8,271 @@ import androidx.compose.foundation.layout.add
 import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.displayCutout
 import androidx.compose.foundation.layout.only
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.systemBars
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Text
 import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.material3.windowsizeclass.WindowHeightSizeClass
 import androidx.compose.material3.windowsizeclass.WindowSizeClass
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.platform.LocalLayoutDirection
 import androidx.compose.ui.unit.Dp
-import androidx.compose.ui.unit.dp
-import com.kaleyra.video_sdk.call.appbar.view.CallAppBarComponent
-import com.kaleyra.video_sdk.call.bottomsheetnew.CallBottomSheetDefaults
-import com.kaleyra.video_sdk.call.bottomsheetnew.CallSheetState
-import com.kaleyra.video_sdk.call.bottomsheetnew.inputmessage.model.InputMessage
+import com.kaleyra.video_common_ui.CallUI
 import com.kaleyra.video_sdk.call.bottomsheetnew.rememberCallSheetState
-import com.kaleyra.video_sdk.call.bottomsheetnew.sheetcontent.VSheetContent
-import com.kaleyra.video_sdk.call.bottomsheetnew.sheetdragcontent.VSheetDragContent
-import com.kaleyra.video_sdk.call.callactions.viewmodel.CallActionsViewModel
-import com.kaleyra.video_sdk.call.callinfowidget.model.Logo
-import com.kaleyra.video_sdk.call.callscreenscaffold.HCallScreenScaffold
-import com.kaleyra.video_sdk.call.streamnew.model.core.StreamUi
-import com.kaleyra.video_sdk.common.immutablecollections.ImmutableList
-import com.kaleyra.video_sdk.extensions.ContextExtensions.findActivity
+import com.kaleyra.video_sdk.call.whiteboard.model.WhiteboardRequest
 import com.kaleyra.video_sdk.theme.KaleyraM3Theme
+import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 internal fun CallScreen(
     windowSizeClass: WindowSizeClass,
-    onBackPressed: () -> Unit,
+    shouldShowFileShareComponent: Boolean,
+    isInPipMode: Boolean,
+    enterPip: () -> Unit,
+    onPipAspectRatio: (Rational) -> Unit,
+    onDisplayMode: (CallUI.DisplayMode) -> Unit,
+    onFileShareVisibility: (Boolean) -> Unit,
+    onWhiteboardVisibility: (Boolean) -> Unit,
+    onUsbCameraConnected: (Boolean) -> Unit,
+    onActivityFinishing: () -> Unit,
+    onAskInputPermissions: (Boolean) -> Unit,
+    onConnectionServicePermissionsResult: () -> Unit,
 ) {
+//    val theme by viewModel.theme.collectAsStateWithLifecycle()
+//    val activity = LocalContext.current.findActivity() as FragmentActivity
+//    val callUiState by viewModel.uiState.collectAsStateWithLifecycle()
+//    val whiteboardRequest by viewModel.whiteboardRequest.collectAsStateWithLifecycle(initialValue = null)
+//    val sheetState = rememberBottomSheetState(
+//        initialValue = BottomSheetValue.Hidden,
+//        collapsable = !callUiState.isAudioOnly,
+//        confirmStateChange = { it != BottomSheetValue.Hidden }
+//    )
+//    val callScreenState = rememberCallScreenState(
+//        sheetState = sheetState,
+//        shouldShowFileShareComponent = shouldShowFileShareComponent
+//    )
+//    // Needed this to handle properly a sequence of multiple permission
+//    // Cannot call micPermissionState.launchPermission followed by cameraPermissionState.launchPermission, or vice versa
+//    val inputPermissionsState = rememberMultiplePermissionsState(permissions = listOf(
+//        RecordAudioPermission, CameraPermission
+//    )) { permissionsResult ->
+//        onAskInputPermissions(false)
+//        permissionsResult.forEach { (permission, isGranted) ->
+//            when {
+//                permission == RecordAudioPermission && isGranted -> viewModel.startMicrophone(activity)
+//                permission == CameraPermission && isGranted -> viewModel.startCamera(activity)
+//            }
+//        }
+//    }
+//    val micPermissionState = rememberPermissionState(permission = RecordAudioPermission) { isGranted ->
+//        onAskInputPermissions(false)
+//        if (isGranted) viewModel.startMicrophone(activity)
+//    }
+//    val cameraPermissionState = rememberPermissionState(permission = CameraPermission) { isGranted ->
+//        onAskInputPermissions(false)
+//        if (isGranted) viewModel.startCamera(activity)
+//    }
+//    val finishActivity = remember(activity) {
+//        {
+//            onActivityFinishing()
+//            activity.finishAndRemoveTask()
+//        }
+//    }
+//    // code executed when pressing the back button in the call ui
+//    // the system back gesture or system back button are handled using the BackHandler composable
+//    val onBackPressed by remember(finishActivity, enterPip) {
+//        derivedStateOf {
+//            {
+//                when {
+//                    callUiState.callState is CallStateUi.Disconnected.Ended -> finishActivity()
+//                    callUiState.fullscreenStream != null -> {
+//                        viewModel.fullscreenStream(null)
+//                    }
+//
+//                    else -> enterPip()
+//                }
+//            }
+//        }
+//    }
+//    LaunchedEffect(isInPipMode, onActivityFinishing) {
+//        viewModel.setOnCallEnded { hasFeedback, hasErrorOccurred, hasBeenKicked ->
+//            onActivityFinishing()
+//            when {
+//                isInPipMode || !activity.isAtLeastResumed() -> activity.finishAndRemoveTask()
+//                !hasFeedback && !hasBeenKicked -> {
+//                    val delayMs = if (hasErrorOccurred) ActivityFinishErrorDelay else ActivityFinishDelay
+//                    delay(delayMs)
+//                    activity.finishAndRemoveTask()
+//                }
+//            }
+//        }
+//    }
+//    LaunchedEffect(onPipAspectRatio) {
+//        viewModel.setOnPipAspectRatio(onPipAspectRatio)
+//    }
+//    LaunchedEffect(onDisplayMode) {
+//        viewModel.setOnDisplayMode(onDisplayMode)
+//    }
+//    val shouldAskConnectionServicePermissions = viewModel.shouldAskConnectionServicePermissions && !activity.hasConnectionServicePermissions()
+//    var shouldAskInputPermissions by remember { mutableStateOf(!shouldAskConnectionServicePermissions) }
+//    val contactsPermissionsState = rememberMultiplePermissionsState(permissions = ContactsPermissions) { _ ->
+//        viewModel.startConnectionService(activity)
+//        shouldAskInputPermissions = true
+//    }
+//    val connectionServicePermissionsState = rememberMultiplePermissionsState(permissions = if (ConnectionServiceUtils.isConnectionServiceSupported) ConnectionServicePermissions else listOf()) { permissionsResult ->
+//        if (permissionsResult.isNotEmpty() && permissionsResult.all { (_, isGranted) -> isGranted }) {
+//            contactsPermissionsState.launchMultiplePermissionRequest()
+//        } else {
+//            viewModel.tryStartCallService()
+//            shouldAskInputPermissions = true
+//        }
+//        onConnectionServicePermissionsResult()
+//    }
+//    if (shouldAskConnectionServicePermissions) {
+//        LaunchedEffect(connectionServicePermissionsState) {
+//            connectionServicePermissionsState.launchMultiplePermissionRequest()
+//        }
+//    }
+//    if (shouldAskInputPermissions) {
+//        LaunchedEffect(inputPermissionsState, micPermissionState, cameraPermissionState) {
+//            viewModel.setOnAudioOrVideoChanged { isAudioEnabled, isVideoEnabled ->
+//                onAskInputPermissions(true)
+//                when {
+//                    isAudioEnabled && isVideoEnabled -> inputPermissionsState.launchMultiplePermissionRequest()
+//                    isAudioEnabled -> micPermissionState.launchPermissionRequest()
+//                    isVideoEnabled -> cameraPermissionState.launchPermissionRequest()
+//                }
+//            }
+//        }
+//    }
+//    LaunchedEffect(onUsbCameraConnected) {
+//        viewModel.setOnUsbCameraConnected(onUsbCameraConnected)
+//    }
+//    CollaborationTheme(theme = theme, transparentSystemBars = true) { isDarkTheme ->
+//        CallScreen(
+//            windowSizeClass = windowSizeClass,
+//            whiteboardRequest = whiteboardRequest,
+//            onThumbnailStreamClick = viewModel::swapThumbnail,
+//            onThumbnailStreamDoubleClick = viewModel::fullscreenStream,
+//            onFullscreenStreamClick = viewModel::fullscreenStream,
+//            onUserFeedback = viewModel::sendUserFeedback,
+//            onConfigurationChange = viewModel::updateStreamsArrangement,
+//            onBackPressed = onBackPressed,
+//            onCallEndedBack = finishActivity,
+//            isInPipMode = isInPipMode,
+//            isDarkTheme = isDarkTheme,
+//            onFileShareVisibility = onFileShareVisibility,
+//            onWhiteboardVisibility = onWhiteboardVisibility,
+//        )
+//    }
+
+    KaleyraM3Theme {
+        CallScreen(
+            windowSizeClass = windowSizeClass,
+            whiteboardRequest = null,
+            shouldShowFileShareComponent = false,
+            onBackPressed = { },
+            onCallEndedBack = { },
+            onFileShareVisibility = { },
+            onWhiteboardVisibility = { },
+        )
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+internal fun CallScreen(
+    windowSizeClass: WindowSizeClass,
+    whiteboardRequest: WhiteboardRequest?,
+    shouldShowFileShareComponent: Boolean,
+    onBackPressed: () -> Unit,
+    isInPipMode: Boolean = false,
+    onCallEndedBack: () -> Unit,
+    onFileShareVisibility: (Boolean) -> Unit,
+    onWhiteboardVisibility: (Boolean) -> Unit
+) {
+    val scope = rememberCoroutineScope()
     val isCompactHeight = windowSizeClass.heightSizeClass == WindowHeightSizeClass.Compact
 
     val sheetState = rememberCallSheetState()
     val modalSheetState = rememberModalBottomSheetState(skipPartiallyExpanded = true)
     var modalSheetComponent: ModalSheetComponent? by remember { mutableStateOf(null) }
+    val onChangeSheetState: (Boolean) -> Unit = remember(sheetState) {
+        { isSheetCollapsed: Boolean ->
+            scope.launch {
+                if (isSheetCollapsed) sheetState.expand()
+                else sheetState.collapse()
+            }
+        }
+    }
 
-    KaleyraM3Theme {
+    var selectedStreamId by remember { mutableStateOf<String?>(null) }
+
+//    FileShareVisibilityObserver(modalSheetComponent, onFileShareVisibility)
+
+//    WhiteboardVisibilityObserver(modalSheetComponent, onWhiteboardVisibility)
+
+//    if (shouldShowFileShareComponent) {
+//        LaunchedEffect(Unit) {
+//            modalSheetComponent = ModalSheetComponent.FileShare
+//        }
+//    }
+
+//    LaunchedEffect(whiteboardRequest) {
+//        when (whiteboardRequest) {
+//            is WhiteboardRequest.Show -> {
+//                if (modalSheetComponent == ModalSheetComponent.Whiteboard) return@LaunchedEffect
+//                modalSheetComponent = ModalSheetComponent.Whiteboard
+//                snapshotFlow { modalSheetComponent }.first { it == ModalSheetComponent.Whiteboard }
+//                CallUserMessagesProvider.sendUserMessage(WhiteboardShowRequestMessage(whiteboardRequest.username))
+//            }
+//            is WhiteboardRequest.Hide -> {
+//                if (modalSheetComponent != ModalSheetComponent.Whiteboard) return@LaunchedEffect
+//                modalSheetComponent = null
+//                snapshotFlow { modalSheetComponent }.first { it == null }
+//                CallUserMessagesProvider.sendUserMessage(WhiteboardHideRequestMessage(whiteboardRequest.username))
+//            }
+//            else -> Unit
+//        }
+//    }
+
+//    val collapseSheet: () -> Unit = remember {
+//        {
+//            scope.launch {
+//                sheetState.collapse()
+//            }
+//        }
+//    }
+
+//    when {
+//        callUiState.callState is CallStateUi.Disconnected.Ended -> BackHandler(onBack = onCallEndedBack)
+//        sheetState.targetValue == CallSheetValue.Expanded -> BackHandler(onBack = collapseSheet)
+//        streamUiState.fullscreenStream != null -> BackHandler(onBack = {
+//            streamViewModel.fullscreen(
+//                null
+//            )
+//        })
+//    }
+
+    if (isInPipMode) {
+        Text("to implement")
+    } else {
         if (isCompactHeight) {
             HCallScreen(
                 windowSizeClass = windowSizeClass,
                 sheetState = sheetState,
                 modalSheetState = modalSheetState,
                 modalSheetComponent = modalSheetComponent,
-                onModalSheetComponentChange = { modalSheetComponent = it },
+                onChangeSheetState = onChangeSheetState,
+                selectedStreamId = selectedStreamId,
+                onStreamSelected = { selectedStreamId = it },
+                onModalSheetComponentRequest = { modalSheetComponent = it },
                 onBackPressed = onBackPressed
             )
         } else {
@@ -73,10 +281,28 @@ internal fun CallScreen(
                 sheetState = sheetState,
                 modalSheetState = modalSheetState,
                 modalSheetComponent = modalSheetComponent,
-                onModalSheetComponentChange = { modalSheetComponent = it },
+                onChangeSheetState = onChangeSheetState,
+                selectedStreamId = selectedStreamId,
+                onStreamSelected = { selectedStreamId = it },
+                onModalSheetComponentRequest = { modalSheetComponent = it },
                 onBackPressed = onBackPressed
             )
         }
+
+//    val callState = callUiState.callState
+//    if (callUiState.showFeedback) {
+//        val activity = LocalContext.current.findActivity() as ComponentActivity
+//        if (activity.isAtLeastResumed()) {
+//            UserFeedbackDialog(onDismiss = onCallEndedBack)
+//        }
+//    }
+
+//    if (callState is CallStateUi.Disconnected.Ended.Kicked) {
+//        val activity = LocalContext.current.findActivity() as ComponentActivity
+//        if (activity.isAtLeastResumed()) {
+//            KickedMessageDialog(adminName = callState.adminName, onDismiss = onCallEndedBack)
+//        }
+//    }
     }
 }
 
@@ -84,44 +310,25 @@ internal const val CompactScreenMaxActions = 5
 internal const val LargeScreenMaxActions = 8
 
 @Composable
-internal fun rememberOnMicClick(callActionsViewModel: CallActionsViewModel): (Boolean) -> Unit {
-    val activity = LocalContext.current.findActivity()
-    return remember(callActionsViewModel) { { _: Boolean -> callActionsViewModel.toggleMic(activity) } }
-}
-
-@Composable
-internal fun rememberOnCameraClick(callActionsViewModel: CallActionsViewModel): (Boolean) -> Unit {
-    val activity = LocalContext.current.findActivity()
-    return remember(callActionsViewModel) {
-        { _: Boolean ->
-            callActionsViewModel.toggleCamera(
-                activity
-            )
-        }
+internal fun FileShareVisibilityObserver(
+    modalSheetComponent: ModalSheetComponent,
+    onFileShareVisibility: (Boolean) -> Unit,
+) {
+    val isFileShareComponent = modalSheetComponent == ModalSheetComponent.FileShare
+    LaunchedEffect(isFileShareComponent) {
+        onFileShareVisibility(isFileShareComponent)
     }
 }
 
 @Composable
-internal fun rememberOnScreenShareClick(
-    callActionsViewModel: CallActionsViewModel,
-    onStartScreenShareClick: () -> Unit,
-): (Boolean) -> Unit {
-    return remember(callActionsViewModel) {
-        { _: Boolean ->
-            if (!callActionsViewModel.tryStopScreenShare()) onStartScreenShareClick()
-        }
+internal fun WhiteboardVisibilityObserver(
+    modalSheetComponent: ModalSheetComponent,
+    onWhiteboardVisibility: (Boolean) -> Unit,
+) {
+    val isWhiteboardComponent = modalSheetComponent == ModalSheetComponent.Whiteboard
+    LaunchedEffect(isWhiteboardComponent) {
+        onWhiteboardVisibility(isWhiteboardComponent)
     }
-}
-
-@Composable
-internal fun rememberOnChatClick(callActionsViewModel: CallActionsViewModel): () -> Unit {
-    val activity = LocalContext.current.findActivity()
-    return remember(callActionsViewModel) { { callActionsViewModel.showChat(activity) } }
-}
-
-@Composable
-internal fun rememberOnHangUpClick(callActionsViewModel: CallActionsViewModel): () -> Unit {
-    return remember(callActionsViewModel) { { callActionsViewModel.hangUp() } }
 }
 
 @Composable
