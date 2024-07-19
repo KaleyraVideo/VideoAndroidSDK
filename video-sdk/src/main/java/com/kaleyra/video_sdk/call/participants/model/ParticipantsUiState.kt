9edd3e03e91eb83f03227a776a578a9a9f0421f1
Stefano Brusadelli <stefano.brusadelli@kaleyra.com>
2024-07-19 12:19:34 +0200

    refactor: add ParticipantsUiState

---
+++ video-sdk/src/main/java/com/kaleyra/video_sdk/call/participants/model/ParticipantsUiState.kt
@@ -0,0 +1,12 @@
+package com.kaleyra.video_sdk.call.participants.model
+
+import androidx.compose.runtime.Immutable
+import com.kaleyra.video_sdk.common.immutablecollections.ImmutableList
+import com.kaleyra.video_sdk.common.uistate.UiState
+
+@Immutable
+internal data class ParticipantsUiState(
+    val invitedParticipants: ImmutableList<String> = ImmutableList(),
+    val adminsStreamsIds: ImmutableList<String> = ImmutableList(),
+    val isLocalParticipantAdmin: Boolean = false,
+): UiState
