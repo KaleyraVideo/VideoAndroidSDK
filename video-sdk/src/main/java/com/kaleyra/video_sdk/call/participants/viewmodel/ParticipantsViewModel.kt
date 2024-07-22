f1343fb8e98ca58f2b968d341bfaf5a18a59776b
Stefano Brusadelli <stefano.brusadelli@kaleyra.com>
2024-07-22 10:16:06 +0200

    refactor: implement ParticipantViewModel

---
+++ video-sdk/src/main/java/com/kaleyra/video_sdk/call/participants/viewmodel/ParticipantsViewModel.kt
@@ -0,0 +1,73 @@
+package com.kaleyra.video_sdk.call.participants.viewmodel
+
+import android.app.Activity
+import androidx.fragment.app.FragmentActivity
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewModelScope
+import com.kaleyra.video.conference.Input
+import com.kaleyra.video.conference.Inputs
+import com.kaleyra.video_common_ui.mapper.ParticipantMapper.toInCallParticipants
+import com.kaleyra.video_sdk.call.participants.model.ParticipantsUiState
+import com.kaleyra.video_sdk.call.viewmodel.BaseViewModel
+import com.kaleyra.video_sdk.common.immutablecollections.toImmutableList
+import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+
+internal class ParticipantsViewModel(configure: suspend () -> Configuration) :
+    BaseViewModel<ParticipantsUiState>(configure) {
+
+    override fun initialState() = ParticipantsUiState()
+
+    init {
+        viewModelScope.launch {
+            val call = call.first()
+
+            combine(
+                call.participants,
+                call.toInCallParticipants()
+            ) { participants, inCallParticipants ->
+                val invited = (listOfNotNull(participants.me) + participants.others - inCallParticipants.toSet()).map { it.userId }
+                _uiState.update {
+                    it.copy(invitedParticipants = invited.toImmutableList())
+                }
+            }.launchIn(this)
+        }
+    }
+
+    // TODO remove code duplication in CallActionsViewModel
+    fun toggleMic(activity: Activity?) {
+        if (activity !is FragmentActivity) return
+        viewModelScope.launch {
+            val inputs = call.getValue()?.inputs
+            val input = inputs?.request(activity, Inputs.Type.Microphone)?.getOrNull<Input.Audio>() ?: return@launch
+            val isMicEnabled = input.enabled.value
+            if (!isMicEnabled) input.tryEnable() else input.tryDisable()
+        }
+    }
+
+    fun muteStreamAudio(streamId: String) {
+        val participants = call.getValue()?.participants?.getValue()?.others
+        val stream = participants?.firstNotNullOfOrNull { other ->
+            other.streams.value.find { it.id == streamId }
+        }
+        val audio = stream?.audio?.getValue() ?: return
+        if (!audio.enabled.value) audio.tryEnable()
+        else audio.tryDisable()
+    }
+
+    companion object {
+
+        fun provideFactory(configure: suspend () -> Configuration) =
+            object : ViewModelProvider.Factory {
+                @Suppress("UNCHECKED_CAST")
+                override fun <T : ViewModel> create(modelClass: Class<T>): T {
+                    return ParticipantsViewModel(configure) as T
+                }
+            }
+    }
+
+}
---
+++ video-sdk/src/test/java/com/kaleyra/video_sdk/viewmodel/call/ParticipantsViewModelTest.kt
@@ -0,0 +1,187 @@
+package com.kaleyra.video_sdk.viewmodel.call
+
+import androidx.fragment.app.FragmentActivity
+import com.kaleyra.video.conference.CallParticipant
+import com.kaleyra.video.conference.CallParticipants
+import com.kaleyra.video.conference.Input
+import com.kaleyra.video.conference.Inputs
+import com.kaleyra.video.conference.Stream
+import com.kaleyra.video_common_ui.CallUI
+import com.kaleyra.video_common_ui.ConferenceUI
+import com.kaleyra.video_common_ui.mapper.ParticipantMapper
+import com.kaleyra.video_common_ui.mapper.ParticipantMapper.toInCallParticipants
+import com.kaleyra.video_sdk.MainDispatcherRule
+import com.kaleyra.video_sdk.call.participants.viewmodel.ParticipantsViewModel
+import com.kaleyra.video_sdk.common.immutablecollections.ImmutableList
+import com.kaleyra.video_sdk.ui.mockkSuccessfulConfiguration
+import io.mockk.coEvery
+import io.mockk.coVerify
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.mockkObject
+import io.mockk.spyk
+import io.mockk.unmockkAll
+import io.mockk.verify
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.test.advanceUntilIdle
+import kotlinx.coroutines.test.runCurrent
+import kotlinx.coroutines.test.runTest
+import org.junit.After
+import org.junit.Assert.assertEquals
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+
+@OptIn(ExperimentalCoroutinesApi::class)
+class ParticipantsViewModelTest {
+
+    @get:Rule
+    var mainDispatcherRule = MainDispatcherRule()
+
+    private val conferenceMock = mockk<ConferenceUI>()
+
+    private val callMock = mockk<CallUI>(relaxed = true)
+
+    private val audioMock1 = mockk<Input.Audio>(relaxed = true)
+
+    private val audioMock2 = mockk<Input.Audio>(relaxed = true)
+
+    private val streamMock1 = mockk<Stream>()
+
+    private val streamMock2 = mockk<Stream>()
+
+    private val meMock = mockk<CallParticipant.Me>()
+
+    private val otherMock1 = mockk<CallParticipant>()
+
+    private val otherMock2 = mockk<CallParticipant>()
+
+    private val participantsMock = mockk<CallParticipants>()
+
+    @Before
+    fun setUp() {
+        mockkObject(ParticipantMapper)
+        every { conferenceMock.call } returns MutableStateFlow(callMock)
+        every { callMock.participants } returns MutableStateFlow(mockk(relaxed = true))
+        every { callMock.toInCallParticipants() } returns MutableStateFlow(listOf())
+
+        every { audioMock1.enabled } returns MutableStateFlow(true)
+        every { audioMock2.enabled } returns MutableStateFlow(true)
+        with(streamMock1) {
+            every { id } returns "streamId1"
+            every { audio } returns MutableStateFlow(audioMock1)
+        }
+        with(streamMock2) {
+            every { id } returns "streamId2"
+            every { audio } returns MutableStateFlow(audioMock2)
+        }
+        every { meMock.userId } returns "userId1"
+        with(otherMock1) {
+            every { userId } returns "userId2"
+            every { streams } returns MutableStateFlow(listOf(streamMock1))
+        }
+        with(otherMock2) {
+            every { userId } returns "userId3"
+            every { streams } returns MutableStateFlow(listOf(streamMock2))
+        }
+        with(participantsMock) {
+            every { me } returns meMock
+            every { others } returns listOf(otherMock1, otherMock2)
+        }
+        every { callMock.participants } returns MutableStateFlow(participantsMock)
+    }
+
+    @After
+    fun tearDown() {
+        unmockkAll()
+    }
+
+    @Test
+    fun testInvitedParticipantsUpdated() = runTest {
+        every { callMock.toInCallParticipants() } returns MutableStateFlow(listOf(otherMock1))
+
+        val viewModel = spyk(ParticipantsViewModel{
+            mockkSuccessfulConfiguration(conference = conferenceMock)
+        })
+        advanceUntilIdle()
+
+        val expected = ImmutableList(listOf(meMock.userId, otherMock2.userId))
+        assertEquals(expected, viewModel.uiState.first().invitedParticipants)
+    }
+
+    @Test
+    fun testToggleMicOn() = runTest {
+        val activity = mockk<FragmentActivity>()
+        val inputs = mockk<Inputs>(relaxed = true)
+        val audio = mockk<Input.Audio>(relaxed = true) {
+            every { enabled } returns MutableStateFlow(false)
+            every { tryEnable() } returns true
+        }
+        every { callMock.inputs } returns inputs
+        coEvery { inputs.request(any(), any()) } returns Inputs.RequestResult.Success(audio)
+
+        val viewModel = spyk(ParticipantsViewModel{
+            mockkSuccessfulConfiguration(conference = conferenceMock)
+        })
+        advanceUntilIdle()
+
+        viewModel.toggleMic(activity)
+        runCurrent()
+
+        coVerify(exactly = 1) { inputs.request(activity, Inputs.Type.Microphone) }
+        verify(exactly = 1) { audio.tryEnable() }
+    }
+
+    @Test
+    fun testToggleMicOff() = runTest {
+        val activity = mockk<FragmentActivity>()
+        val inputs = mockk<Inputs>(relaxed = true)
+        val audio = mockk<Input.Audio>(relaxed = true) {
+            every { enabled } returns MutableStateFlow(true)
+            every { tryDisable() } returns true
+        }
+        every { callMock.inputs } returns inputs
+        coEvery { inputs.request(any(), any()) } returns Inputs.RequestResult.Success(audio)
+
+        val viewModel = spyk(ParticipantsViewModel{
+            mockkSuccessfulConfiguration(conference = conferenceMock)
+        })
+        advanceUntilIdle()
+
+        viewModel.toggleMic(activity)
+        runCurrent()
+
+        coVerify(exactly = 1) { inputs.request(activity, Inputs.Type.Microphone) }
+        verify(exactly = 1) { audio.tryDisable() }
+    }
+
+    @Test
+    fun testMuteStreamAudio() = runTest {
+        every { audioMock2.enabled } returns MutableStateFlow(true)
+
+        val viewModel = spyk(ParticipantsViewModel{
+            mockkSuccessfulConfiguration(conference = conferenceMock)
+        })
+        advanceUntilIdle()
+
+        viewModel.muteStreamAudio(streamMock2.id)
+
+        verify(exactly = 1) { audioMock2.tryDisable() }
+    }
+
+    @Test
+    fun testUnmuteStreamAudio() = runTest {
+        every { audioMock2.enabled } returns MutableStateFlow(false)
+
+        val viewModel = spyk(ParticipantsViewModel{
+            mockkSuccessfulConfiguration(conference = conferenceMock)
+        })
+        advanceUntilIdle()
+
+        viewModel.muteStreamAudio(streamMock2.id)
+
+        verify(exactly = 1) { audioMock2.tryEnable() }
+    }
+}
