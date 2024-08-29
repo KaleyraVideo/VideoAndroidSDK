e50deca56582dcb7e43823f291644f4b4f4748b1
Federico Marin <federico.marin@tatacommunications.com>
2024-08-29 15:13:22 +0200

    chore: remove unused class

--- video-sdk/src/main/java/com/kaleyra/video_sdk/common/snackbar/view/StackedSnackbarExt.kt
+++
@@ -1,9 +0,0 @@
-package com.kaleyra.video_sdk.common.snackbar.view
-
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
-import androidx.compose.ui.platform.AccessibilityManager
-import com.kaleyra.video_sdk.common.snackbar.model.StackedSnackbarHostMessagesHandler
-
-@Composable
-fun rememberStackedSnackbarHostState(accessibilityManager: AccessibilityManager) = remember { StackedSnackbarHostMessagesHandler(accessibilityManager) }
