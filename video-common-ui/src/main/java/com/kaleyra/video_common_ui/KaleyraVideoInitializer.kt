651f3d37ca6eb351285e1e316c73c74094b75507
Federico Marin <federico.marin@tatacommunications.com>
2024-09-04 13:01:01 +0200

    chore: update KaleyraVideoInitializer code doc

--- video-common-ui/src/main/java/com/kaleyra/video_common_ui/KaleyraVideoInitializer.kt
+++ video-common-ui/src/main/java/com/kaleyra/video_common_ui/KaleyraVideoInitializer.kt
@@ -4,23 +4,24 @@ import android.content.Context
 
 /**
  * KaleyraVideoInitializer
- * Representation of a class capable of being started from within KaleyraVideoSDK and being used to configure KaleyraVideoSDK when required
+ *
+ * Representation of a class capable of being started from within KaleyraVideoSDK and being used to configure and connect KaleyraVideoSDK when required.
  */
 abstract class KaleyraVideoInitializer {
 
     /**
-     * Application context provided when the class is instantiated useful for SDK configuration and connection purposes
+     * Application context provided when the class is instantiated from within the SDK. Useful for SDK configuration and connection purposes.
      */
     lateinit var applicationContext: Context
         internal set
 
     /**
-     * Callback that is invoked when the KaleyraVideoSDK is required to be configured in order to let KaleyraVideoSDK function properly
+     * Callback that is invoked when the KaleyraVideoSDK is required to be configured in order to let KaleyraVideoSDK function properly.
      */
     abstract fun onRequestKaleyraVideoConfigure()
 
     /**
-     * Callback that is invoked when the KaleyraVideoSDK is required to be connected to let KaleyraVideoSDK function properly
+     * Callback that is invoked when the KaleyraVideoSDK is required to be connected to let KaleyraVideoSDK function properly.
      */
     abstract fun onRequestKaleyraVideoConnect()
 }
