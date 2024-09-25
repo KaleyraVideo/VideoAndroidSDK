f24f3940a6485d45532ece2c91186701cc138842
Stefano Brusadelli <stefano.brusadelli@kaleyra.com>
2024-09-25 10:48:54 +0200

    refactor: update StreamKt composable for the right fade effect

--- video-sdk/src/androidTest/java/com/kaleyra/video_sdk/call/stream/StreamTest.kt
+++ video-sdk/src/androidTest/java/com/kaleyra/video_sdk/call/stream/StreamTest.kt
@@ -1,6 +1,6 @@
 package com.kaleyra.video_sdk.call.stream
 
-import android.view.View
+import android.graphics.Matrix
 import androidx.activity.ComponentActivity
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -9,19 +9,33 @@ import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
 import androidx.compose.ui.test.onNodeWithTag
 import androidx.compose.ui.test.onNodeWithText
+import androidx.test.platform.app.InstrumentationRegistry
+import com.kaleyra.video.conference.Input
+import com.kaleyra.video.conference.StreamView
 import com.kaleyra.video.conference.VideoStreamView
 import com.kaleyra.video_sdk.call.stream.model.core.ImmutableView
+import com.kaleyra.video_sdk.call.stream.view.core.RenderingDebouceMillis
 import com.kaleyra.video_sdk.call.stream.view.core.Stream
 import com.kaleyra.video_sdk.call.stream.view.core.StreamViewTestTag
+import com.kaleyra.video_utils.MutableSharedStateFlow
+import kotlinx.coroutines.flow.MutableStateFlow
 import org.junit.After
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
+import kotlin.reflect.full.memberProperties
+import kotlin.reflect.jvm.javaField
 
 class StreamTest {
 
     @get:Rule
     val composeTestRule = createAndroidComposeRule<ComponentActivity>()
+
+    private val instrumentation = InstrumentationRegistry.getInstrumentation()
+
+    private val context = instrumentation.context
+
+    private var streamView by mutableStateOf<ImmutableView<VideoStreamView>?>(null)
 
     private var showStreamView by mutableStateOf(true)
 
@@ -29,7 +43,7 @@ class StreamTest {
     fun setUp() {
         composeTestRule.setContent {
             Stream(
-                streamView = ImmutableView(VideoStreamView(composeTestRule.activity)),
+                streamView = streamView,
                 username = "username",
                 avatar = null,
                 showStreamView = showStreamView,
@@ -39,16 +53,19 @@ class StreamTest {
 
     @After
     fun tearDown() {
+        streamView = null
         showStreamView = true
     }
 
     @Test
     fun streamViewIsDisplayed() {
+        instrumentation.runOnMainSync { streamView = ImmutableView(VideoStreamView(context)) }
         composeTestRule.onNodeWithTag(StreamViewTestTag).assertIsDisplayed()
     }
 
     @Test
     fun showStreamViewTrue_avatarIsNotDisplayed() {
+        instrumentation.runOnMainSync { streamView = ImmutableView(VideoStreamView(context)) }
         showStreamView = true
         composeTestRule.onNodeWithText("U").assertDoesNotExist()
     }
@@ -57,5 +74,55 @@ class StreamTest {
     fun showStreamViewFalse_avatarIsDisplayed() {
         showStreamView = false
         composeTestRule.onNodeWithText("U").assertIsDisplayed()
+    }
+
+    @Test
+    fun testAvatarIsForceDisplayedIfStreamViewIsNotRendering() {
+        val renderingFlow = MutableSharedStateFlow<StreamView.State>(StreamView.State.NotRendering)
+        showStreamView = true
+        instrumentation.runOnMainSync {
+            val videoStreamView = VideoStreamView(context)
+            val property =
+                videoStreamView::class.memberProperties.first { it.name == "internalState" }
+            val field = property.javaField!!
+            field.isAccessible = true
+            field.set(videoStreamView, renderingFlow)
+            streamView = ImmutableView(videoStreamView)
+        }
+        composeTestRule.onNodeWithText("U").assertDoesNotExist()
+        composeTestRule.mainClock.advanceTimeBy(RenderingDebouceMillis)
+        composeTestRule.onNodeWithText("U").assertIsDisplayed()
+
+        renderingFlow.value = StreamView.State.Rendering(
+            definition = MutableStateFlow(Input.Video.Quality.Definition.HD),
+            matrix = MutableStateFlow(Matrix())
+        )
+        composeTestRule.mainClock.advanceTimeBy(RenderingDebouceMillis)
+        composeTestRule.onNodeWithText("U").assertDoesNotExist()
+    }
+
+    @Test
+    fun testAvatarIsNotForceDisplayedIfStreamViewIsRendering() {
+        showStreamView = true
+        instrumentation.runOnMainSync {
+            val videoStreamView = VideoStreamView(context)
+            val property =
+                videoStreamView::class.memberProperties.first { it.name == "internalState" }
+            val field = property.javaField!!
+            field.isAccessible = true
+            field.set(
+                videoStreamView,
+                MutableSharedStateFlow(
+                    StreamView.State.Rendering(
+                        definition = MutableStateFlow(Input.Video.Quality.Definition.HD),
+                        matrix = MutableStateFlow(Matrix())
+                    )
+                )
+            )
+            streamView = ImmutableView(videoStreamView)
+        }
+        composeTestRule.onNodeWithText("U").assertDoesNotExist()
+        composeTestRule.mainClock.advanceTimeBy(RenderingDebouceMillis)
+        composeTestRule.onNodeWithText("U").assertDoesNotExist()
     }
 }
--- video-sdk/src/main/java/com/kaleyra/video_sdk/call/stream/view/core/Stream.kt
+++ video-sdk/src/main/java/com/kaleyra/video_sdk/call/stream/view/core/Stream.kt
@@ -4,11 +4,13 @@ package com.kaleyra.video_sdk.call.stream.view.core
 
 import android.view.ViewGroup
 import androidx.annotation.DrawableRes
-import androidx.compose.animation.AnimatedContent
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.EnterTransition
 import androidx.compose.animation.ExitTransition
+import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.core.tween
 import androidx.compose.animation.fadeIn
-import androidx.compose.animation.togetherWith
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
@@ -16,10 +18,10 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.unit.coerceIn
 import androidx.compose.ui.unit.dp
@@ -33,14 +35,11 @@ import com.kaleyra.video_sdk.common.avatar.model.ImmutableUri
 import com.kaleyra.video_sdk.common.avatar.view.Avatar
 import kotlinx.coroutines.FlowPreview
 import kotlinx.coroutines.flow.debounce
-import kotlinx.coroutines.flow.firstOrNull
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.onEach
-import kotlinx.coroutines.launch
 
 const val StreamViewTestTag = "StreamViewTestTag"
-private const val StreamViewAnimationDurationMillis = 150L
 internal const val RenderingDebouceMillis = 1000L
 
 @Composable
@@ -54,43 +53,25 @@ internal fun Stream(
     onClick: (() -> Unit)? = null,
     avatarModifier: Modifier = Modifier
 ) {
-    val scope = rememberCoroutineScope()
     var forceDisplayAvatar by remember { mutableStateOf(false) }
-    var isFirstComposition by remember { mutableStateOf(true) }
+    var isRendering by remember { mutableStateOf(false) }
+    val streamViewAlpha by animateFloatAsState(
+        targetValue = if (isRendering) 1f else 0f,
+        animationSpec = tween(),
+        label = "stream view alpha"
+    )
 
-    AnimatedContent(
-        targetState = !showStreamView || streamView == null || forceDisplayAvatar,
-        transitionSpec = {
-            fadeIn(animationSpec = tween()).togetherWith(ExitTransition.None)
-        },
-        label = "stream content",
-        modifier = modifier
-    ) { shouldDisplayAvatar ->
-        if (shouldDisplayAvatar) {
-            StreamAvatar(
-                username,
-                avatar,
-                avatarPlaceholder = avatarPlaceholder,
-                modifier = avatarModifier
-            )
-        } else {
+    Box(modifier) {
+        // Do not remove the stream view if forceDisplayAvatar is true,
+        // otherwise the stream view's rendering state would be always StreamView.State.NotRendering,
+        // since the view need to be attached in order to start rendering
+        if (showStreamView && streamView != null) {
             AndroidView(
                 factory = {
-                    streamView!!.value.apply {
+                    streamView.value.apply {
                         val parentView = parent as? ViewGroup
                         parentView?.removeView(this)
                         setOnClickListener { onClick?.invoke() }
-                        // Animate the stream view with a fading effect if
-                        // the animated content is started with the android view.
-                        if (isFirstComposition) {
-                            alpha = 0f
-                            scope.launch {
-                                streamView.value.state.firstOrNull { it is StreamView.State.Rendering }
-                                animate()
-                                    .setDuration(StreamViewAnimationDurationMillis)
-                                    .alpha(1f)
-                            }
-                        }
                     }
                 },
                 update = { view ->
@@ -99,23 +80,40 @@ internal fun Stream(
                     newLayoutParams.width = ViewGroup.LayoutParams.MATCH_PARENT
                     view.layoutParams = newLayoutParams
                 },
-                modifier = Modifier.testTag(StreamViewTestTag)
+                modifier = Modifier
+                    .testTag(StreamViewTestTag)
+                    .graphicsLayer { alpha = streamViewAlpha }
             )
-
-            LaunchedEffect(Unit) {
-                streamView!!.value.state
-                    .map { it is StreamView.State.Rendering }
-                    // This debounce is used for two purposes:
-                    // 1. If the view doesn't render within the timeout period, the avatar is displayed as a fallback.
-                    // 2. It also sets a minimum display time for the avatar if it was set as fallback for the previous case,
-                    // ensuring it remains visible for this duration even if the stream is rendered successfully.
-                    .debounce { if (it) 0 else RenderingDebouceMillis }
-                    .onEach { forceDisplayAvatar = !it }
-                    .launchIn(this)
-            }
         }
-        LaunchedEffect(Unit) {
-            isFirstComposition = false
+
+
+        AnimatedVisibility(
+            visible = !showStreamView || streamView == null || forceDisplayAvatar,
+            // Animate only if the avatar is shown because of forceDisplayAvatar true
+            enter = if (forceDisplayAvatar) fadeIn(tween()) else EnterTransition.None,
+            exit = ExitTransition.None
+        ) {
+            StreamAvatar(
+                username,
+                avatar,
+                avatarPlaceholder = avatarPlaceholder,
+                modifier = avatarModifier
+            )
+        }
+    }
+
+    if (streamView != null) {
+        LaunchedEffect(streamView) {
+            streamView.value.state
+                .map { it is StreamView.State.Rendering }
+                .onEach { isRendering = it }
+                // This debounce is used for two purposes:
+                // 1. If the view doesn't render within the timeout period, the avatar is displayed as a fallback.
+                // 2. It also sets a minimum display time for the avatar if it was set as fallback for the previous case,
+                // ensuring it remains visible for this duration even if the stream is rendered successfully.
+                .debounce(RenderingDebouceMillis)
+                .onEach { forceDisplayAvatar = !it }
+                .launchIn(this)
         }
     }
 }
