aa3b665da7158c404fea5c9d6b1bdcb211ca06f4
Federico Marin <federico.marin@tatacommunications.com>
2024-10-29 19:26:16 +0100

    chore: use demo app injected logo

--- app/src/main/java/com/kaleyra/demo_video_sdk/DemoAppKaleyraVideoInitializer.kt
+++ app/src/main/java/com/kaleyra/demo_video_sdk/DemoAppKaleyraVideoInitializer.kt
@@ -35,6 +35,7 @@ import com.kaleyra.video_common_ui.model.UserDetails
 import com.kaleyra.video_common_ui.model.UserDetailsProvider
 import com.kaleyra.video_common_ui.theme.Theme
 import com.kaleyra.video_common_ui.theme.resource.ColorResource
+import com.kaleyra.video_common_ui.theme.resource.URIResource
 import com.kaleyra.video_common_ui.utils.InputsExtensions.useBackCamera
 import kotlinx.coroutines.MainScope
 import kotlinx.coroutines.flow.launchIn
@@ -52,7 +53,10 @@ class DemoAppKaleyraVideoInitializer : KaleyraVideoInitializer() {
                 KaleyraVideo.conference.connectionServiceOption = ConnectionServiceOption.Enabled
                 KaleyraVideo.pushNotificationHandlingStrategy = PushNotificationHandlingStrategy.Automatic
                 val colorResourceSeed = ColorResource(Color(0xFF2A638A).toArgb())
+                val appConfiguration = ConfigurationPrefsManager.getConfiguration(applicationContext)
+                val logoUri = appConfiguration.logoUrl?.let { Uri.parse(it) } ?: Uri.EMPTY
                 KaleyraVideo.theme = Theme(
+                    logo = Theme.Logo(URIResource(logoUri, logoUri)),
                     palette = Theme.Palette(seed = colorResourceSeed),
                     typography = Theme.Typography(fontFamily = KaleyraFontFamily.default),
                     config = Theme.Config(style = Theme.Config.Style.System)
