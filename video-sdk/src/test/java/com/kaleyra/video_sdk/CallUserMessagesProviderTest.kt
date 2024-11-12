a19d30e9f3051d0907bc2d4a9c2b129d5a09aadb
Federico Marin <federico.marin@tatacommunications.com>
2024-11-12 01:02:19 +0100

    chore: update tests

--- video-sdk/src/test/java/com/kaleyra/video_sdk/CallUserMessagesProviderTest.kt
+++ video-sdk/src/test/java/com/kaleyra/video_sdk/CallUserMessagesProviderTest.kt
@@ -45,8 +45,11 @@ import org.junit.After
 import org.junit.Assert.assertEquals
 import org.junit.Before
 import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
 
 @OptIn(ExperimentalCoroutinesApi::class)
+@RunWith(RobolectricTestRunner::class)
 class CallUserMessagesProviderTest {
 
     private val callMock = mockk<CallUI>(relaxed = true)
