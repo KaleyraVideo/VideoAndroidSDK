6aecb375bb7f2901f41e55a1d4a830cf76472cc3
Stefano Brusadelli <stefano.brusadelli@kaleyra.com>
2025-01-13 13:08:47 +0100

    chore: add FullscreenLayoutTest

---
+++ video-sdk/src/test/java/com/kaleyra/video_sdk/layout/FullscreenLayoutImplTest.kt
@@ -0,0 +1,16 @@
+package com.kaleyra.video_sdk.call.stream.viewmodel
+
+import org.junit.Assert.*
+
+import org.junit.Test
+
+class FullscreenLayoutImplTest {
+
+    @Test
+    fun setFullscreenStream() {
+    }
+
+    @Test
+    fun clearFullscreenStream() {
+    }
+}
