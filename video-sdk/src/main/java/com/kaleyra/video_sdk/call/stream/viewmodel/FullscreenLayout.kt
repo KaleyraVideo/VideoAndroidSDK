9d66b40c6c2e51373be4a4b0f430844c52dc3148
Stefano Brusadelli <stefano.brusadelli@kaleyra.com>
2025-01-13 13:10:02 +0100

    refactor: Fullscreen layout

--- video-sdk/src/main/java/com/kaleyra/video_sdk/call/stream/viewmodel/FullscreenLayout.kt
+++ video-sdk/src/main/java/com/kaleyra/video_sdk/call/stream/viewmodel/FullscreenLayout.kt
@@ -1,5 +1,6 @@
 package com.kaleyra.video_sdk.call.stream.viewmodel
 
+import android.util.Log
 import com.kaleyra.video_sdk.call.stream.model.core.StreamUi
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -8,37 +9,48 @@ import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.distinctUntilChanged
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.flow.update
 
-internal interface MaximizedStreamLayout: StreamLayout
+internal interface FullscreenLayout : StreamLayout {
 
-internal class MaximizedStreamLayoutImpl(
+    fun setFullscreenStream(streamId: String)
+
+    fun clearFullscreenStream()
+}
+
+internal class FullscreenLayoutImpl(
     override val streams: StateFlow<List<StreamUi>>,
     coroutineScope: CoroutineScope
-): MaximizedStreamLayout {
+): FullscreenLayout {
 
-    private val maximizedStreamId = MutableStateFlow<String?>(null)
+    private val _fullscreenStreamId = MutableStateFlow<String?>(null)
 
-    private val _streamsPresentation: MutableStateFlow<List<StreamPresentation>> = MutableStateFlow(emptyList())
+    private val _streamItems: MutableStateFlow<List<StreamItem>> = MutableStateFlow(emptyList())
 
-    override val streamsPresentation: StateFlow<List<StreamPresentation>> = _streamsPresentation
+    override val streamItems: StateFlow<List<StreamItem>> = _streamItems
 
     init {
         combine(
             streams,
-            maximizedStreamId
-        ) { streams, maximizedStreamId ->
-            streams.find { it.id == maximizedStreamId }?.let { listOf(StreamPresentation(it)) } ?: emptyList()
+            _fullscreenStreamId
+        ) { allStreams, fullscreenStreamId ->
+            val fullscreenStream = allStreams.firstOrNull { it.id == fullscreenStreamId }
+            listOfNotNull(
+                fullscreenStream?.let { stream ->
+                    StreamItem.Stream(stream.id, stream, state = StreamItemState.FULLSCREEN)
+                }
+            )
         }
             .distinctUntilChanged()
-            .onEach { presentation -> _streamsPresentation.value = presentation }
+            .onEach { streamItems -> _streamItems.update { streamItems } }
             .launchIn(coroutineScope)
     }
 
-    fun maximize(streamId: String) {
-        maximizedStreamId.value = streamId
+    override fun setFullscreenStream(streamId: String) {
+        _fullscreenStreamId.value = streamId
     }
 
-    fun clear() {
-        maximizedStreamId.value = null
+    override fun clearFullscreenStream() {
+        _fullscreenStreamId.value = null
     }
 }
