b3cadf68eda70d1f1afb5abf9a07718acdf41967
Stefano Brusadelli <stefano.brusadelli@kaleyra.com>
2025-01-17 16:51:15 +0100

    chore: add AutoLayout implementation

--- video-sdk/src/main/java/com/kaleyra/video_sdk/call/stream/viewmodel/AutoLayout.kt
+++ video-sdk/src/main/java/com/kaleyra/video_sdk/call/stream/viewmodel/AutoLayout.kt
@@ -1,205 +1,172 @@
 package com.kaleyra.video_sdk.call.stream.viewmodel
 
 import androidx.compose.runtime.Immutable
+import androidx.compose.runtime.Stable
 import com.kaleyra.video_sdk.call.stream.model.core.StreamUi
 import com.kaleyra.video_sdk.call.stream.utils.isMyCameraStream
 import com.kaleyra.video_sdk.call.stream.utils.isRemoteCameraStream
 import com.kaleyra.video_sdk.call.stream.utils.isRemoteScreenShare
-import com.kaleyra.video_sdk.common.usermessages.model.PinScreenshareMessage
-import com.kaleyra.video_sdk.common.usermessages.provider.CallUserMessagesProvider
+import com.kaleyra.video_sdk.common.avatar.model.ImmutableUri
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.flow.combine
-import kotlinx.coroutines.flow.distinctUntilChanged
 import kotlinx.coroutines.flow.launchIn
-import kotlinx.coroutines.flow.map
-import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 
 internal interface StreamLayout {
 
     val streams: StateFlow<List<StreamUi>>
 
-    val streamsPresentation: StateFlow<List<StreamPresentation>>
+    val streamItems: StateFlow<List<StreamItem>>
 }
 
 internal interface AutoLayout : StreamLayout {
 
-    val callUserMessageProvider: CallUserMessagesProvider
+    val isOneToOneCall: StateFlow<Boolean>
 
-    val isOto: StateFlow<Boolean>
+    val maxAllowedFeaturedStreams: StateFlow<Int>
 
-    val maxFeaturedStreams: StateFlow<Int>
+    val maxAllowedThumbnailStreams: StateFlow<Int>
 
     val isDefaultBackCamera: Boolean
 }
 
+sealed class StreamItemState {
+    data object Standard : StreamItemState()
+
+    sealed class Featured : StreamItemState() {
+
+        companion object Companion : Featured()
+
+        data object Pinned : Featured()
+
+        data object Fullscreen : Featured()
+    }
+
+    data object Thumbnail : StreamItemState()
+}
+
+
 @Immutable
-data class StreamPresentation(
-    val stream: StreamUi,
-    val isPromoted: Boolean = false,
+data class UserPreview(
+    val username: String,
+    val avatar: ImmutableUri?,
 )
 
-class AutoLayoutImpl(
+@Stable
+internal sealed interface StreamItem {
+
+    val id: String
+
+    @Immutable
+    data class Stream(
+        override val id: String,
+        val stream: StreamUi,
+        val state: StreamItemState = StreamItemState.Standard,
+    ) : StreamItem
+
+    @Immutable
+    data class More(
+        override val id: String = "moreItemId",
+        val users: List<UserPreview>,
+    ) : StreamItem
+}
+
+internal class AutoLayoutImpl(
     override val streams: StateFlow<List<StreamUi>>,
-    override val isOto: StateFlow<Boolean>,
-    override val maxFeaturedStreams: StateFlow<Int>,
+    override val isOneToOneCall: StateFlow<Boolean>,
+    override val maxAllowedFeaturedStreams: StateFlow<Int>,
+    override val maxAllowedThumbnailStreams: StateFlow<Int>,
     override val isDefaultBackCamera: Boolean,
-    override val callUserMessageProvider: CallUserMessagesProvider,
     coroutineScope: CoroutineScope,
 ) : AutoLayout {
 
-    /**
-     * Represents the internal state of the AutoLayout.
-     *
-     * @property allStreams All streams received.
-     * @property displayedStreams Streams currently displayed.
-     * @property isOneToOneMode Indicates if the layout is in one-to-one mode.
-     * @property maxFeaturedStreams Maximum number of featured streams.
-     */
     private data class LayoutState(
         val allStreams: List<StreamUi> = emptyList(),
-        val displayedStreams: List<StreamPresentation> = emptyList(),
-        val isOneToOneMode: Boolean = true,
-        val maxFeaturedStreams: Int = 0
+        val isOneToOneCall: Boolean = false,
+        val maxAllowedFeaturedStreams: Int = 0,
+        val maxAllowedThumbnailStreams: Int = 0,
     )
 
     private val _internalState: MutableStateFlow<LayoutState> = MutableStateFlow(LayoutState())
 
-    private val _streamsPresentation: MutableStateFlow<List<StreamPresentation>> = MutableStateFlow(emptyList())
+    private val _streamItems: MutableStateFlow<List<StreamItem>> = MutableStateFlow(emptyList())
 
-    override val streamsPresentation: StateFlow<List<StreamPresentation>> = _streamsPresentation
+    override val streamItems: StateFlow<List<StreamItem>> = _streamItems
 
     init {
         combine(
             streams,
-            isOto,
-            maxFeaturedStreams
-        ) { streams, isOto, maxFeaturedStreams ->
-            _internalState.update { currentState ->
-                val newDisplayedStreams = determineDisplayedStreams(currentState, streams, isOto, maxFeaturedStreams, callUserMessageProvider)
-                currentState.copy(
+            isOneToOneCall,
+            maxAllowedFeaturedStreams,
+            maxAllowedThumbnailStreams
+        ) { streams, isOneToOneCall, maxAllowedFeaturedStreams, maxAllowedThumbnailStreams ->
+            _internalState.update { state ->
+                state.copy(
                     allStreams = streams,
-                    displayedStreams = newDisplayedStreams,
-                    isOneToOneMode = isOto,
-                    maxFeaturedStreams = maxFeaturedStreams
+                    isOneToOneCall = isOneToOneCall,
+                    maxAllowedFeaturedStreams = maxAllowedFeaturedStreams,
+                    maxAllowedThumbnailStreams = maxAllowedThumbnailStreams
                 )
             }
         }.launchIn(coroutineScope)
 
-        _internalState
-            .map { it.displayedStreams }
-            .distinctUntilChanged()
-            .onEach { _streamsPresentation.value = it }
-            .launchIn(coroutineScope)
-    }
+        coroutineScope.launch {
+            _internalState.collectLatest { state ->
+                val streamItemsProvider = if (shouldArrangeByPriority(state, _streamItems.value)) {
+                    val featuredStreamId = findCurrentFeaturedStreamItem(_streamItems.value)?.takeIf { it.stream.video?.isScreenShare == true }?.id
+                    val sortedStreams = sortStreamsByPriority(
+                        streams = state.allStreams,
+                        featuredStreamId = featuredStreamId
+                    )
 
-    /**
-     * Determines the list of streams to be displayed in the UI and their presentation order.
-     *
-     * This function decides how to arrange and present a list of streams based on various factors,
-     * including whether the call is in one-to-one mode, the presence of remote screen shares,
-     * and the current state of the layout. It also handles the logic for requesting to pin
-     * new remote screen shares.
-     *
-     * @param currentState The current state of the layout, including the currently displayed streams.
-     * @param streams The list of available streams to be displayed.
-     * @param isOto `true` if the call is in one-to-one mode, `false` otherwise.
-     * @param maxFeaturedStreams The maximum number of streams that can be displayed in a featured manner.
-     * @param callUserMessageProvider A provider for sending messages to the user or other parts of the application.
-     * @return A list of [StreamPresentation] objects representing the streams to be displayed and their order.
-     *
-     * The function follows these steps:
-     * 1. Counts the number of remote screen shares in the provided `streams`.
-     * 2. Determines if streams should be prioritized based on the following conditions:
-     *    - The call is in one-to-one mode (`isOto`).
-     *    - There is exactly one remote screen share.
-     *    - There are no remote screen shares, and the default camera is the back camera.
-     * 3. If streams should be prioritized:
-     *    - Calls `arrangeStreamsByPriority` to arrange the streams in a prioritized order.
-     * 4. If streams should not be prioritized:
-     *    - Attempts to find a new remote screen share that has not been displayed before using `findNewRemoteScreenShares`.
-     *    - If a new screen share is found:
-     *      - Sends a message to the user via `callUserMessageProvider` to request pinning the new screen share.
-     *      - Calls `arrangeStreamsByPriority` again, passing the ID of the currently promoted stream (if any) to potentially maintain its prominence.
-     *    - If no new screen share is found:
-     *      - Calls `arrangeStreamsInGrid` to arrange the streams in a grid layout, limited by `maxFeaturedStreams`.
-     */
-    private fun determineDisplayedStreams(
-        currentState: LayoutState,
-        streams: List<StreamUi>,
-        isOto: Boolean,
-        maxFeaturedStreams: Int,
-        callUserMessageProvider: CallUserMessagesProvider
-    ): List<StreamPresentation> {
-        val remoteScreenShareCount = streams.count { it.isRemoteScreenShare() }
-        val shouldPrioritize = isOto || remoteScreenShareCount == 1 || (remoteScreenShareCount == 0 && isDefaultBackCamera)
-        return if (shouldPrioritize) {
-            arrangeStreamsByPriority(streams)
-        } else {
-            val screenShareToRequestPin = findNewRemoteScreenShares(currentState, streams).firstOrNull()
-            if (screenShareToRequestPin != null) {
-                callUserMessageProvider.sendUserMessage(PinScreenshareMessage(screenShareToRequestPin.id, screenShareToRequestPin.username))
-                arrangeStreamsByPriority(
-                    streams,
-                    currentState.displayedStreams.find { it.isPromoted }?.stream?.id
-                )
-            } else arrangeStreamsInGrid(streams, maxFeaturedStreams)
+                    FeaturedStreamItemsProvider(
+                        streams = sortedStreams,
+                        featuredStreamIds = listOfNotNull(sortedStreams.firstOrNull()?.id),
+                        maxAllowedThumbnailStreams = state.maxAllowedThumbnailStreams,
+                    )
+                } else {
+                    MosaicStreamItemsProvider(state.allStreams, state.maxAllowedFeaturedStreams)
+                }
+                _streamItems.update { streamItemsProvider.buildStreamItems() }
+            }
         }
     }
 
-    /**
-     * Arranges streams based on a priority order.
-     *
-     * @param streams The list of streams to arrange.
-     * @param priorityStreamId The ID of the stream to prioritize (if any).
-     * @return A list of StreamPresentation objects in the desired order.
-     */
-    private fun arrangeStreamsByPriority(streams: List<StreamUi>, priorityStreamId: String? = null): List<StreamPresentation> {
+    private fun findCurrentFeaturedStreamItem(items: List<StreamItem>): StreamItem.Stream? {
+        return items
+            .filterIsInstance<StreamItem.Stream>()
+            .firstOrNull { it.state == StreamItemState.Featured }
+    }
+
+    private fun shouldArrangeByPriority(
+        state: LayoutState,
+        currentStreamItems: List<StreamItem>,
+    ): Boolean {
+        val newRemoteScreenShareCount = state.allStreams.count { it.isRemoteScreenShare() }
+        val previousRemoteScreenShareCount = currentStreamItems
+            .filterIsInstance<StreamItem.Stream>()
+            .count { it.stream.isRemoteScreenShare() }
+
+        val hasOneRemoteScreenShare = newRemoteScreenShareCount == 1
+        val hasTwoRemoteScreensWithStreamItems =
+            newRemoteScreenShareCount > 1 && previousRemoteScreenShareCount != 0
+        return state.isOneToOneCall || hasOneRemoteScreenShare || hasTwoRemoteScreensWithStreamItems
+    }
+
+    private fun sortStreamsByPriority(
+        streams: List<StreamUi>,
+        featuredStreamId: String? = null,
+    ): List<StreamUi> {
         return streams
             .sortedWith(
-                compareByDescending<StreamUi> { it.id == priorityStreamId }
+                compareByDescending<StreamUi> { it.id == featuredStreamId }
                     .thenByDescending { it.isRemoteScreenShare() }
                     .thenByDescending { it.isMyCameraStream() && isDefaultBackCamera }
                     .thenByDescending { it.isRemoteCameraStream() }
             )
-            .mapIndexed { index, stream -> StreamPresentation(stream, isPromoted = index == 0) }
     }
-
-    /**
-     * Arranges streams in a grid layout, taking only the specified maximum number of featured streams.
-     *
-     * @param streams The list of streams to arrange.
-     * @param maxFeaturedStreams The maximum number of streams to feature.
-     * @return A list of StreamPresentation objects in the grid layout.
-     */
-    private fun arrangeStreamsInGrid(streams: List<StreamUi>, maxFeaturedStreams: Int): List<StreamPresentation> {
-        return streams.take(maxFeaturedStreams).map { StreamPresentation(it) }
-    }
-
-    /**
-     * Finds new remote screen shares that have not been displayed before.
-     *
-     * @param currentState The current state of the pin layout.
-     * @param newStreams The new list of available streams.
-     * @return A list of new remote screen shares.
-     */
-    private fun findNewRemoteScreenShares(currentState: LayoutState, newStreams: List<StreamUi>): List<StreamUi> {
-        val displayedStreamIds = currentState.allStreams.map { it.id }
-        return newStreams.filter { isNewRemoteScreenShare(it, displayedStreamIds) }
-    }
-
-    /**
-     * Determines if a stream is a new remote screen share.
-     *
-     * @param stream The stream to check.
-     * @param presentedStreamIds The IDs of streams that have already been presented.
-     * @return True if the stream is a new remote screen share, false otherwise.
-     */
-    private fun isNewRemoteScreenShare(
-        stream: StreamUi,
-        presentedStreamIds: List<String>,
-    ): Boolean = stream.isRemoteScreenShare() && stream.id !in presentedStreamIds
 }
-
