59529059e171e043a56fb0e1d9367ca2b40caa48
Stefano Brusadelli <stefano.brusadelli@kaleyra.com>
2025-01-17 16:55:36 +0100

    refactor: add VideoLayoutController

--- video-sdk/src/main/java/com/kaleyra/video_sdk/call/stream/viewmodel/VideoLayoutController.kt
+++ video-sdk/src/main/java/com/kaleyra/video_sdk/call/stream/viewmodel/VideoLayoutController.kt
@@ -1,70 +1,167 @@
 package com.kaleyra.video_sdk.call.stream.viewmodel
 
+import android.util.Log
 import com.kaleyra.video_sdk.call.stream.model.core.StreamUi
+import com.kaleyra.video_sdk.call.stream.utils.isRemoteScreenShare
+import com.kaleyra.video_sdk.common.usermessages.model.PinScreenshareMessage
 import com.kaleyra.video_sdk.common.usermessages.provider.CallUserMessagesProvider
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.flatMapLatest
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.flow.update
 
-interface VideoLayoutController {
+internal interface VideoLayoutController {
 
     val streams: StateFlow<List<StreamUi>>
 
-    val streamsPresentation: StateFlow<List<StreamPresentation>>
+    val isOneToOneCall: StateFlow<Boolean>
 
     val maxFeaturedStreams: StateFlow<Int>
 
     val maxAllowedPinnedStreams: StateFlow<Int>
 
-    val callUserMessageProvider: CallUserMessagesProvider
+    val maxAllowedThumbnailStreams: StateFlow<Int>
 
-    val isOneToOne: StateFlow<Boolean>
+    val streamItems: StateFlow<List<StreamItem>>
+
+    val callUserMessageProvider: CallUserMessagesProvider
 
     val isDefaultBackCamera: Boolean
 
-    fun pin(streamId: String, prepend: Boolean = false, force: Boolean = false): Boolean
+    fun enterMosaicLayout()
 
-    fun unpin(streamId: String)
+    fun enterAutoLayout()
 
-    fun enableFullscreen(id: String)
+    fun pinStream(streamId: String, prepend: Boolean = false, force: Boolean = false): Boolean
 
-    fun exitFullscreen()
+    fun unpinStream(streamId: String)
+
+    fun setFullscreenStream(id: String)
+
+    fun clearFullscreenStream()
 }
 
-class VideoLayoutControllerImpl(
+internal class VideoLayoutControllerImpl(
     override val streams: StateFlow<List<StreamUi>>,
+    override val isOneToOneCall: StateFlow<Boolean>,
     override val maxFeaturedStreams: StateFlow<Int>,
     override val maxAllowedPinnedStreams: StateFlow<Int>,
+    override val maxAllowedThumbnailStreams: StateFlow<Int>,
     override val isDefaultBackCamera: Boolean,
-    override val isOneToOne: StateFlow<Boolean>,
+    // TODO implement this logic
     override val callUserMessageProvider: CallUserMessagesProvider,
     coroutineScope: CoroutineScope,
 ) : VideoLayoutController {
 
-    private val autoLayout = AutoLayoutImpl(streams, isOneToOne , maxFeaturedStreams, isDefaultBackCamera, callUserMessageProvider, coroutineScope)
+    private val autoLayout: AutoLayout = AutoLayoutImpl(
+        streams = streams,
+        isOneToOneCall = isOneToOneCall,
+        maxAllowedFeaturedStreams = maxFeaturedStreams,
+        maxAllowedThumbnailStreams = maxAllowedThumbnailStreams,
+        isDefaultBackCamera = isDefaultBackCamera,
+        coroutineScope = coroutineScope
+    )
 
-    private val gridLayout = GridLayoutImpl(streams, maxFeaturedStreams, coroutineScope)
+    private val manualLayout: ManualLayout = ManualLayoutImpl(
+        streams = streams,
+        maxAllowedFeaturedStreams = maxFeaturedStreams,
+        maxAllowedPinnedStreams = maxAllowedPinnedStreams,
+        maxAllowedThumbnailStreams = maxAllowedThumbnailStreams,
+        coroutineScope = coroutineScope
+    )
 
-    private val pinLayout = PinLayoutImpl(streams, maxAllowedPinnedStreams, callUserMessageProvider, coroutineScope)
+    private data class ControllerState(
+        val streamLayout: StreamLayout,
+        val previousStreamLayout: StreamLayout? = null,
+        val allStreamsWithVideo: List<StreamUi> = emptyList()
+    )
 
-    private val maximizedStreamLayout = MaximizedStreamLayoutImpl(streams, coroutineScope)
+    private val _internalState: MutableStateFlow<ControllerState> = MutableStateFlow(ControllerState(autoLayout))
 
-    override val streamsPresentation: StateFlow<List<StreamPresentation>> = autoLayout.streamsPresentation
+    override val streamItems: StateFlow<List<StreamItem>> = _internalState
+        .flatMapLatest { it.streamLayout.streamItems }
+        .stateIn(
+            scope = coroutineScope,
+            started = SharingStarted.WhileSubscribed(5000L),
+            initialValue = emptyList()
+        )
 
-    override fun pin(streamId: String, prepend: Boolean, force: Boolean): Boolean {
-        pinLayout.pin(streamId, prepend, force)
-        return false
+    init {
+        streams
+            .onEach { streams ->
+                val streamsWithVideo = streams.filter { it.video != null }
+                _internalState.update { state ->
+                    if (state.streamLayout is ManualLayout || (state.streamLayout is AutoLayout && streams.count { it.isRemoteScreenShare() } > 1)) {
+                        val screenShareToRequestPin = findNewRemoteScreenShares(
+                            state,
+                            streamsWithVideo
+                        ).firstOrNull()
+                        screenShareToRequestPin?.let {
+                            callUserMessageProvider.sendUserMessage(
+                                PinScreenshareMessage(it.id, it.username)
+                            )
+                        }
+                    }
+                    state.copy(allStreamsWithVideo = streamsWithVideo)
+                }
+
+            }.launchIn(coroutineScope)
     }
 
-    override fun unpin(streamId: String) {
-
+    override fun enterMosaicLayout() {
+        manualLayout.clearPinnedStreams()
+        updateInternalState(manualLayout)
     }
 
-    override fun enableFullscreen(id: String) {
-        maximizedStreamLayout.maximize(id)
+    override fun enterAutoLayout() {
+        updateInternalState(autoLayout)
     }
 
-    override fun exitFullscreen() {
-        // TODO restore previous layout
-        maximizedStreamLayout.clear()
+    override fun pinStream(streamId: String, prepend: Boolean, force: Boolean): Boolean {
+        val isPinned = manualLayout.pinStream(streamId, prepend, force)
+        updateInternalState(manualLayout)
+        return isPinned
     }
+
+    override fun unpinStream(streamId: String) {
+        manualLayout.unpinStream(streamId)
+    }
+
+    override fun setFullscreenStream(id: String) {
+        manualLayout.setFullscreenStream(id)
+        updateInternalState(manualLayout)
+    }
+
+    override fun clearFullscreenStream() {
+        manualLayout.clearFullscreenStream()
+        _internalState.value.previousStreamLayout?.let { layout ->
+            updateInternalState(layout)
+        }
+    }
+
+    private fun findNewRemoteScreenShares(currentState: ControllerState, newStreams: List<StreamUi>): List<StreamUi> {
+        val displayedStreamIds = currentState.allStreamsWithVideo.map { it.id }
+        val a = newStreams.filter { isNewRemoteScreenShare(it, displayedStreamIds) }
+        return a
+    }
+
+    private fun isNewRemoteScreenShare(
+        stream: StreamUi,
+        presentedStreamIds: List<String>,
+    ): Boolean = stream.isRemoteScreenShare() && stream.id !in presentedStreamIds
+
+    private fun updateInternalState(newLayout: StreamLayout) {
+        _internalState.update { state ->
+            state.copy(
+                streamLayout = newLayout,
+                previousStreamLayout = state.streamLayout
+            )
+        }
+    }
+
 }
