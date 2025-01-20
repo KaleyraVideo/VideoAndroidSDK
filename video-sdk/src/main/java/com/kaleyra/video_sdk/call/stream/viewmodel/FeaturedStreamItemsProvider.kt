301da79dd70840ae7c87ee31da66367af3b1270d
Stefano Brusadelli <stefano.brusadelli@kaleyra.com>
2025-01-20 16:01:59 +0100

    refactor: FeaturedStreamItemsProvider and its tests

--- video-sdk/src/main/java/com/kaleyra/video_sdk/call/stream/viewmodel/FeaturedStreamItemsProvider.kt
+++ video-sdk/src/main/java/com/kaleyra/video_sdk/call/stream/viewmodel/FeaturedStreamItemsProvider.kt
@@ -5,37 +5,36 @@ import com.kaleyra.video_sdk.call.stream.model.core.StreamUi
 internal class FeaturedStreamItemsProvider(
     override val streams: List<StreamUi>,
     private val featuredStreamIds: List<String>,
-    private val maxAllowedThumbnailStreams: Int,
+    private val maxThumbnailStreams: Int,
     private val featuredStreamItemState: StreamItemState.Featured = StreamItemState.Featured
 ): StreamItemsProvider {
 
     override fun buildStreamItems(): List<StreamItem> {
+        require(maxThumbnailStreams >= 0) {
+            "maxAllowedThumbnailStreams must be greater or equal to 0"
+        }
         if (featuredStreamIds.isEmpty()) return emptyList()
 
         val (featuredStreams, nonFeaturedStreams) = streams.partition { it.id in featuredStreamIds }
 
         val featuredStreamIdIndices = featuredStreamIds.withIndex().associate { indexedValue -> indexedValue.value to indexedValue.index }
         val sortedFeaturedStreams = featuredStreams.sortedBy { featuredStreamIdIndices[it.id] ?: Int.MAX_VALUE }
-        return if (nonFeaturedStreams.size <= maxAllowedThumbnailStreams) {
-            (sortedFeaturedStreams + nonFeaturedStreams).map { stream ->
-                StreamItem.Stream(
-                    id = stream.id,
-                    stream = stream,
-                    state = if (stream in featuredStreams) featuredStreamItemState else StreamItemState.Thumbnail
-                )
-            }
+        val (thumbnailStreams, moreStreams) = if (nonFeaturedStreams.size <= maxThumbnailStreams) {
+            nonFeaturedStreams to emptyList()
         } else {
-            val (thumbnailStreams, remainingStreams) = nonFeaturedStreams.withIndex().partition { it.index < maxAllowedThumbnailStreams - 1 }
-
-            val featuredStreamItems = sortedFeaturedStreams.map { StreamItem.Stream(it.id, it, state = featuredStreamItemState) }
-            val thumbnailStreamItems = thumbnailStreams.map { indexedValue ->
-                StreamItem.Stream(indexedValue.value.id, indexedValue.value, state = StreamItemState.Thumbnail)
-            }
-            val moreItem = StreamItem.More(
-                users = remainingStreams.map { UserPreview(it.value.username, it.value.avatar) }
-            )
-
-            featuredStreamItems + thumbnailStreamItems + moreItem
+            with (nonFeaturedStreams) { take(maxThumbnailStreams - 1) to drop(maxThumbnailStreams - 1) }
         }
+
+        val featuredStreamItems = sortedFeaturedStreams.map {
+            StreamItem.Stream(it.id, it, state = featuredStreamItemState)
+        }
+        val thumbnailStreamItems = thumbnailStreams.map {
+            StreamItem.Stream(it.id, it, state = StreamItemState.Thumbnail)
+        }
+        val moreStreamItems = moreStreams.takeIf { it.isNotEmpty() }?.let { streams ->
+            StreamItem.More(users = streams.map { UserPreview(it.username, it.avatar) })
+        }
+
+        return featuredStreamItems + thumbnailStreamItems + listOfNotNull(moreStreamItems)
     }
 }
--- video-sdk/src/test/java/com/kaleyra/video_sdk/layout/FeaturedStreamItemsProviderTest.kt
+++ video-sdk/src/test/java/com/kaleyra/video_sdk/layout/FeaturedStreamItemsProviderTest.kt
@@ -1,4 +1,172 @@
 package com.kaleyra.video_sdk.layout
 
+import com.kaleyra.video_sdk.call.stream.model.core.StreamUi
+import com.kaleyra.video_sdk.call.stream.viewmodel.FeaturedStreamItemsProvider
+import com.kaleyra.video_sdk.call.stream.viewmodel.StreamItem
+import com.kaleyra.video_sdk.call.stream.viewmodel.StreamItemState
+import com.kaleyra.video_sdk.call.stream.viewmodel.UserPreview
+import com.kaleyra.video_sdk.common.avatar.model.ImmutableUri
+import io.mockk.mockk
+import org.junit.Assert
+import org.junit.Test
+
 class FeaturedStreamItemsProviderTest {
+
+    @Test
+    fun `buildStreamItems returns empty list when featuredStreamIds is empty`() {
+        val streams = listOf(
+            StreamUi("1", "Stream 1"),
+            StreamUi("2", "Stream 2")
+        )
+        val featuredStreamIds = emptyList<String>()
+        val maxThumbnailStreams = 2
+        val provider = FeaturedStreamItemsProvider(streams, featuredStreamIds, maxThumbnailStreams)
+
+        val result = provider.buildStreamItems()
+
+        Assert.assertEquals(emptyList<StreamItem>(), result)
+    }
+
+    @Test
+    fun `buildStreamItems returns all streams as featured when all are in featuredStreamIds`() {
+        val streams = listOf(
+            StreamUi("1", "Stream 1"),
+            StreamUi("2", "Stream 2"),
+            StreamUi("3", "Stream 3")
+        )
+        val featuredStreamIds = listOf("1", "2", "3")
+        val maxThumbnailStreams = 1
+        val provider = FeaturedStreamItemsProvider(streams, featuredStreamIds, maxThumbnailStreams)
+
+        val result = provider.buildStreamItems()
+
+        Assert.assertEquals(3, result.size)
+        Assert.assertEquals(StreamItem.Stream("1", StreamUi("1", "Stream 1"), StreamItemState.Featured), result[0])
+        Assert.assertEquals(StreamItem.Stream("2", StreamUi("2", "Stream 2"), StreamItemState.Featured), result[1])
+        Assert.assertEquals(StreamItem.Stream("3", StreamUi("3", "Stream 3"), StreamItemState.Featured), result[2])
+    }
+
+    @Test(expected = IllegalArgumentException::class)
+    fun `buildStreamItems throws an IllegalArgumentException if maxThumbnailStreams is less than zero`() {
+        val streams = listOf(
+            StreamUi("1", "Stream 1"),
+            StreamUi("2", "Stream 2"),
+            StreamUi("3", "Stream 3")
+        )
+        val featuredStreamIds = listOf("1")
+        val maxThumbnailStreams = -4
+        val provider = FeaturedStreamItemsProvider(streams, featuredStreamIds, maxThumbnailStreams)
+        provider.buildStreamItems()
+    }
+
+    @Test
+    fun `buildStreamItems returns featured and thumbnail streams when non featured streams are less or equal than maxThumbnailStreams`() {
+        val streams = listOf(
+            StreamUi("1", "Stream 1"),
+            StreamUi("2", "Stream 2"),
+            StreamUi("3", "Stream 3")
+        )
+        val featuredStreamIds = listOf("1")
+        val maxThumbnailStreams = 2
+        val provider = FeaturedStreamItemsProvider(streams, featuredStreamIds, maxThumbnailStreams)
+
+        val result = provider.buildStreamItems()
+
+        Assert.assertEquals(3, result.size)
+        Assert.assertEquals(StreamItem.Stream("1", StreamUi("1", "Stream 1"), StreamItemState.Featured), result[0])
+        Assert.assertEquals(StreamItem.Stream("2", StreamUi("2", "Stream 2"), StreamItemState.Thumbnail), result[1])
+        Assert.assertEquals(StreamItem.Stream("3", StreamUi("3", "Stream 3"), StreamItemState.Thumbnail), result[2])
+    }
+
+    @Test
+    fun `buildStreamItems returns featured, thumbnail, and more when non featured streams are greater than maxThumbnailStreams`() {
+        val avatar1 = ImmutableUri(mockk())
+        val avatar2 = ImmutableUri(mockk())
+        val streams = listOf(
+            StreamUi("1", "Stream 1"),
+            StreamUi("2", "Stream 2"),
+            StreamUi("3", "Stream 3", avatar = avatar1),
+            StreamUi("4", "Stream 4", avatar = avatar2)
+        )
+        val featuredStreamIds = listOf("1")
+        val maxThumbnailStreams = 2
+        val provider = FeaturedStreamItemsProvider(streams, featuredStreamIds, maxThumbnailStreams)
+
+        val result = provider.buildStreamItems()
+
+        Assert.assertEquals(3, result.size)
+        Assert.assertEquals(StreamItem.Stream("1", StreamUi("1", "Stream 1"), StreamItemState.Featured), result[0])
+        Assert.assertEquals(StreamItem.Stream("2", StreamUi("2", "Stream 2"), StreamItemState.Thumbnail), result[1])
+        Assert.assertEquals(StreamItem.More(users = listOf(UserPreview("Stream 3", avatar1), UserPreview("Stream 4", avatar2))), result[2])
+    }
+
+    @Test
+    fun `buildStreamItems sorts featured streams according to featuredStreamIds order`() {
+        val streams = listOf(
+            StreamUi("1", "Stream 1"),
+            StreamUi("2", "Stream 2"),
+            StreamUi("3", "Stream 3")
+        )
+        val featuredStreamIds = listOf("3", "1")
+        val maxThumbnailStreams = 2
+        val provider = FeaturedStreamItemsProvider(streams, featuredStreamIds, maxThumbnailStreams)
+
+        val result = provider.buildStreamItems()
+
+        Assert.assertEquals(3, result.size)
+        Assert.assertEquals(StreamItem.Stream("3", StreamUi("3", "Stream 3"), StreamItemState.Featured), result[0])
+        Assert.assertEquals(StreamItem.Stream("1", StreamUi("1", "Stream 1"), StreamItemState.Featured), result[1])
+        Assert.assertEquals(StreamItem.Stream("2", StreamUi("2", "Stream 2"), StreamItemState.Thumbnail), result[2])
+    }
+
+    @Test
+    fun `buildStreamItems handles featuredStreamIds with missing stream ids`() {
+        val streams = listOf(
+            StreamUi("1", "Stream 1"),
+            StreamUi("2", "Stream 2")
+        )
+        val featuredStreamIds = listOf("3", "1")
+        val maxThumbnailStreams = 2
+        val provider = FeaturedStreamItemsProvider(streams, featuredStreamIds, maxThumbnailStreams)
+
+        val result = provider.buildStreamItems()
+
+        Assert.assertEquals(2, result.size)
+        Assert.assertEquals(StreamItem.Stream("1", StreamUi("1", "Stream 1"), StreamItemState.Featured), result[0])
+        Assert.assertEquals(StreamItem.Stream("2", StreamUi("2", "Stream 2"), StreamItemState.Thumbnail), result[1])
+    }
+
+    @Test
+    fun `buildStreamItems handles featuredStreamIds with duplicated stream ids`() {
+        val streams = listOf(
+            StreamUi("1", "Stream 1"),
+            StreamUi("2", "Stream 2")
+        )
+        val featuredStreamIds = listOf("1")
+        val maxThumbnailStreams = 2
+        val provider = FeaturedStreamItemsProvider(streams, featuredStreamIds, maxThumbnailStreams)
+
+        val result = provider.buildStreamItems()
+
+        Assert.assertEquals(2, result.size)
+        Assert.assertEquals(StreamItem.Stream("1", StreamUi("1", "Stream 1"), StreamItemState.Featured), result[0])
+        Assert.assertEquals(StreamItem.Stream("2", StreamUi("2", "Stream 2"), StreamItemState.Thumbnail), result[1])
+    }
+
+    @Test
+    fun `featured streams' state is the provided featuredStreamItemState`() {
+        val streams = listOf(
+            StreamUi("1", "Stream 1"),
+            StreamUi("2", "Stream 2")
+        )
+        val featuredStreamIds = listOf("1")
+        val maxThumbnailStreams = 2
+        val provider = FeaturedStreamItemsProvider(streams, featuredStreamIds, maxThumbnailStreams, featuredStreamItemState = StreamItemState.Featured.Pinned)
+
+        val result = provider.buildStreamItems()
+
+        Assert.assertEquals(2, result.size)
+        Assert.assertEquals(StreamItem.Stream("1", StreamUi("1", "Stream 1"), StreamItemState.Featured.Pinned), result[0])
+        Assert.assertEquals(StreamItem.Stream("2", StreamUi("2", "Stream 2"), StreamItemState.Thumbnail), result[1])
+    }
 }
