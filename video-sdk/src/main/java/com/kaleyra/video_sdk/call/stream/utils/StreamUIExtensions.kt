02885273f64f1f041839e028ea71ca86c4da8998
Stefano Brusadelli <stefano.brusadelli@kaleyra.com>
2025-01-23 17:53:37 +0100

    refactor: StreamUIExtensions and add its tests

--- video-sdk/src/main/java/com/kaleyra/video_sdk/call/stream/utils/StreamUIExtensions.kt
+++ video-sdk/src/main/java/com/kaleyra/video_sdk/call/stream/utils/StreamUIExtensions.kt
@@ -2,9 +2,11 @@ package com.kaleyra.video_sdk.call.stream.utils
 
 import com.kaleyra.video_sdk.call.stream.model.core.StreamUi
 
-internal fun StreamUi.isRemoteScreenShare(): Boolean = !isMine && video?.isScreenShare == true
+internal fun StreamUi.hasVideo(): Boolean = video != null
 
-internal fun StreamUi.isLocalScreenShare(): Boolean = isMine && video?.isScreenShare == true
+internal fun StreamUi.isRemoteScreenShare(): Boolean = !isMine && video != null && video.isScreenShare
+
+internal fun StreamUi.isLocalScreenShare(): Boolean = isMine && video != null && video.isScreenShare
 
 internal fun StreamUi.isMyCameraStream(): Boolean = isMine && video != null && !video.isScreenShare
 
