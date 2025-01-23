24ee3094c46087851137235bd4931da5d03559c3
Stefano Brusadelli <stefano.brusadelli@kaleyra.com>
2025-01-23 17:31:18 +0100

    refactor: update StreamUiState

--- video-sdk/src/main/java/com/kaleyra/video_sdk/call/stream/model/StreamUiState.kt
+++ video-sdk/src/main/java/com/kaleyra/video_sdk/call/stream/model/StreamUiState.kt
@@ -7,15 +7,19 @@ import com.kaleyra.video_sdk.common.uistate.UiState
 /**
  * Represents the UI state for displaying a collection of streams.
  *
- * This class holds information about the active stream preview (if any) and a list of
- * stream items to be displayed in the UI.
+ * This class holds information about the active stream preview (if any), a list of
+ * stream items to be displayed in the UI, and whether the maximum number of pinned streams
+ * has been reached.
  *
  * @property preview The currently active stream preview, or `null` if no preview is active.
  * @property streamItems An immutable list of [StreamItem] objects representing the streams
  * to be displayed.
+ * @property hasReachedMaxPinnedStreams `true` if the maximum number of pinned streams has
+ * been reached, `false` otherwise.
  */
 @Immutable
 internal data class StreamUiState(
     val preview: StreamPreview? = null,
     val streamItems: ImmutableList<StreamItem> = ImmutableList(),
+    val hasReachedMaxPinnedStreams: Boolean = false
 ): UiState
