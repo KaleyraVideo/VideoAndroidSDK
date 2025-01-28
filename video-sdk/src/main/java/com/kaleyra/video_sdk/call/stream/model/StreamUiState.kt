bcae5aa3999fc7e86d61bebad371c12d27a272cd
Stefano Brusadelli <stefano.brusadelli@kaleyra.com>
2025-01-28 17:47:28 +0100

    chore: update StreamUiState

--- video-sdk/src/main/java/com/kaleyra/video_sdk/call/stream/model/StreamUiState.kt
+++ video-sdk/src/main/java/com/kaleyra/video_sdk/call/stream/model/StreamUiState.kt
@@ -5,21 +5,23 @@ import com.kaleyra.video_sdk.common.immutablecollections.ImmutableList
 import com.kaleyra.video_sdk.common.uistate.UiState
 
 /**
- * Represents the UI state for displaying a collection of streams.
+ * Represents the UI state for displaying a collection of streams, including screen share status.
  *
  * This class holds information about the active stream preview (if any), a list of
- * stream items to be displayed in the UI, and whether the maximum number of pinned streams
- * has been reached.
+ * stream items to be displayed in the UI, whether the maximum number of pinned streams
+ * has been reached, and whether screen sharing is currently active.
  *
  * @property preview The currently active stream preview, or `null` if no preview is active.
  * @property streamItems An immutable list of [StreamItem] objects representing the streams
  * to be displayed.
  * @property hasReachedMaxPinnedStreams `true` if the maximum number of pinned streams has
  * been reached, `false` otherwise.
+ * @property isScreenShareActive `true` if screen sharing is currently active, `false` otherwise.
  */
 @Immutable
 internal data class StreamUiState(
     val preview: StreamPreview? = null,
     val streamItems: ImmutableList<StreamItem> = ImmutableList(),
-    val hasReachedMaxPinnedStreams: Boolean = false
+    val hasReachedMaxPinnedStreams: Boolean = false,
+    val isScreenShareActive: Boolean = false
 ): UiState
