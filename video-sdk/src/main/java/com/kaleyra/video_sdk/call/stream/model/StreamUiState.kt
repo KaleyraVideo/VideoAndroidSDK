7e86e87bf42d2923a44e942064776109ab232fdd
Stefano Brusadelli <stefano.brusadelli@kaleyra.com>
2025-02-03 15:11:46 +0100

    refactor: update StreamUiState by adding areStreamsInAutoMode

--- video-sdk/src/main/java/com/kaleyra/video_sdk/call/stream/model/StreamUiState.kt
+++ video-sdk/src/main/java/com/kaleyra/video_sdk/call/stream/model/StreamUiState.kt
@@ -5,23 +5,26 @@ import com.kaleyra.video_sdk.common.immutablecollections.ImmutableList
 import com.kaleyra.video_sdk.common.uistate.UiState
 
 /**
- * Represents the UI state for displaying a collection of streams, including screen share status.
+ * Represents the UI state for displaying a collection of streams, including screen share status and automatic stream management.
  *
  * This class holds information about the active stream preview (if any), a list of
  * stream items to be displayed in the UI, whether the maximum number of pinned streams
- * has been reached, and whether screen sharing is currently active.
+ * has been reached, whether screen sharing is currently active, and whether the stream items are being managed automatically.
  *
  * @property preview The currently active stream preview, or `null` if no preview is active.
  * @property streamItems An immutable list of [StreamItem] objects representing the streams
- * to be displayed.
+ * to be displayed. Defaults to an empty list.
  * @property hasReachedMaxPinnedStreams `true` if the maximum number of pinned streams has
  * been reached, `false` otherwise.
  * @property isScreenShareActive `true` if screen sharing is currently active, `false` otherwise.
+ * @property areStreamsInAutoMode `true` if the `streamItems` are being managed automatically by the system, `false` if they are being managed manually.
+ * When in auto mode, the system may automatically populate, update, filter, or sort the `streamItems`.
  */
 @Immutable
 internal data class StreamUiState(
     val preview: StreamPreview? = null,
     val streamItems: ImmutableList<StreamItem> = ImmutableList(),
     val hasReachedMaxPinnedStreams: Boolean = false,
-    val isScreenShareActive: Boolean = false
-): UiState
+    val isScreenShareActive: Boolean = false,
+    val areStreamsInAutoMode: Boolean = false,
+) : UiState
