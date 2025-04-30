5168bcda566c5f5ad92a486eba5118619204f3fb
Federico Marin <federico.marin@tatacommunications.com>
2025-04-30 11:09:57 +0200

    fix: consider terms and condition procedure as completed as soon as conference is connected

--- video-sdk/src/main/java/com/kaleyra/video_sdk/termsandconditions/viewmodel/TermsAndConditionsViewModel.kt
+++ video-sdk/src/main/java/com/kaleyra/video_sdk/termsandconditions/viewmodel/TermsAndConditionsViewModel.kt
@@ -22,6 +22,7 @@ import androidx.lifecycle.viewModelScope
 import com.kaleyra.video.State
 import com.kaleyra.video_sdk.call.viewmodel.BaseViewModel
 import com.kaleyra.video_sdk.termsandconditions.model.TermsAndConditionsUiState
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.flatMapLatest
 import kotlinx.coroutines.flow.launchIn
@@ -33,17 +34,14 @@ import kotlinx.coroutines.flow.update
  * Terms And Conditions View Model
  * @constructor
  */
+@ExperimentalCoroutinesApi
 class TermsAndConditionsViewModel(configure: suspend () -> Configuration) : BaseViewModel<TermsAndConditionsUiState>(configure) {
 
     override fun initialState() = TermsAndConditionsUiState()
 
     init {
         val conferenceState = conference.flatMapLatest { it.state }
-        val conversationState = conversation.flatMapLatest { it.state }
-        combine(conferenceState, conversationState) { pbState, cbState ->
-            pbState != State.Connecting && cbState != State.Connecting
-        }
-            .takeWhile { !it }
+        conferenceState.takeWhile { it !is State.Connected }
             .onCompletion { _uiState.update { it.copy(isConnected = true) } }
             .launchIn(viewModelScope)
     }
