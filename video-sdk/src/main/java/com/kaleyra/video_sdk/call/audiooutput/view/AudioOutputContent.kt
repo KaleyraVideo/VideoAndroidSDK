a1e461f4c2ceb72052fbc3e89a5bc62ffc634efd
Stefano Brusadelli <stefano.brusadelli@kaleyra.com>
2025-05-21 11:49:01 +0200

    feat: add auto output keyboard accessibility

--- video-sdk/src/main/java/com/kaleyra/video_sdk/call/audiooutput/view/AudioOutputContent.kt
+++ video-sdk/src/main/java/com/kaleyra/video_sdk/call/audiooutput/view/AudioOutputContent.kt
@@ -17,7 +17,9 @@
 package com.kaleyra.video_sdk.call.audiooutput.view
 
 import android.content.res.Configuration
+import androidx.compose.foundation.LocalIndication
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -25,6 +27,7 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.semantics.Role
 import androidx.compose.ui.tooling.preview.Preview
@@ -32,6 +35,7 @@ import androidx.compose.ui.unit.dp
 import com.kaleyra.video_sdk.call.audiooutput.model.AudioDeviceUi
 import com.kaleyra.video_sdk.call.audiooutput.model.mockAudioDevices
 import com.kaleyra.video_sdk.common.immutablecollections.ImmutableList
+import com.kaleyra.video_sdk.extensions.ModifierExtensions.highlightOnFocus
 import com.kaleyra.video_sdk.theme.KaleyraTheme
 
 @Composable
@@ -44,6 +48,7 @@ internal fun AudioOutputContent(
         contentPadding = PaddingValues(vertical = 16.dp))
     {
         items(items = items.value.distinctBy { it.id }, key = { it.id }) {
+            val interactionSource = remember { MutableInteractionSource() }
             AudioOutputItem(
                 audioDevice = it,
                 selected = it.id == playingDeviceId,
@@ -51,8 +56,11 @@ internal fun AudioOutputContent(
                     .clickable(
                         onClickLabel = clickLabelFor(it),
                         role = Role.Button,
-                        onClick = { onItemClick(it) }
+                        onClick = { onItemClick(it) },
+                        indication = LocalIndication.current,
+                        interactionSource = interactionSource
                     )
+                    .highlightOnFocus(interactionSource)
                     .fillMaxWidth()
                     .padding(horizontal = 24.dp,  vertical = 12.dp)
             )
