607f2f5cd140d59f3f50b0e2b86e6aef3160a528
Stefano Brusadelli <stefano.brusadelli@kaleyra.com>
2025-06-03 15:21:35 +0200

    feat: add keyboard accessibility for UserMessageSnackbar

--- video-sdk/src/main/java/com/kaleyra/video_sdk/common/snackbar/view/UserMessageSnackBarM3.kt
+++ video-sdk/src/main/java/com/kaleyra/video_sdk/common/snackbar/view/UserMessageSnackBarM3.kt
@@ -9,7 +9,6 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.heightIn
@@ -27,6 +26,7 @@ import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.Stable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -34,6 +34,8 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.platform.LocalContext
@@ -67,9 +69,13 @@ internal fun UserMessageSnackbarM3(
     contentColor: Color = MaterialTheme.colorScheme.surface
 ) {
     var isMultilineMessage by remember { mutableStateOf(false) }
+    val focusRequester = remember { FocusRequester() }
 
     Box(
-        modifier = Modifier.fillMaxWidth().padding(horizontal = 16.dp)
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 16.dp)
+            .focusRequester(focusRequester)
     ) {
         Row(modifier = modifier
             .background(
@@ -147,6 +153,10 @@ internal fun UserMessageSnackbarM3(
                 )
             }
         }
+    }
+
+    LaunchedEffect(Unit) {
+        focusRequester.requestFocus()
     }
 }
 
